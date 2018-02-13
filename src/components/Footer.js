eb5945ec5015b45d3c3bfdccfd3497621df5bffd
syntaxval <syntaxval@users.noreply.github.com>
2018-02-13 20:41:59 +0100

    Bump to 0.1.19

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,20 @@
-import React, {Component} from 'react'
-import './Footer.css'
+import React, {Component} from "react"
+import "./Footer.css"
 
 
 export default class Footer extends Component {
-  render() {
-    return (
-      <div className='footer'>
-        <div className="flex-row-space-between">
-          <div>
-            © <span>Stellar Fox</span> 2017-2018.
-          </div>
-          <div>
-            ver. 0.1.18
-          </div>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="footer">
+                <div className="flex-row-space-between">
+                    <div>
+                        © <span>Stellar Fox</span> 2017-2018.
+                    </div>
+                    <div>
+                        ver. 0.1.19
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
