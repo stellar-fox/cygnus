298184ac183e21a2d4a857ebee2022fdbf6a2165
drmats <drmats@users.noreply.github.com>
2018-02-15 13:37:11 +0100

    Lint: Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,47 +1,50 @@
-import React, {Component} from 'react'
-import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
-import {connect} from 'react-redux'
-import './Layout.css'
-import Header from './Header'
-import Content from './Content'
-import Footer from './Footer'
-import Welcome from './Welcome'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import stellarTheme from '../frontend/themes/stellar'
+import React, { Component } from "react"
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch,
+} from "react-router-dom"
+import { connect } from "react-redux"
+import Header from "./Header"
+import Content from "./Content"
+import Footer from "./Footer"
+import Welcome from "./Welcome"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import stellarTheme from "../frontend/themes/stellar"
+import "./Layout.css"
 
 class Layout extends Component {
-  render() {
-    return (
-      <MuiThemeProvider muiTheme={stellarTheme}>
-        <div>
-          <Router>
-            <div>
-              {this.props.auth.isHorizonLoggedIn ?
-                (
-                  <div>
-                    <Header/>
-                    <Content />
-                    <Footer />
-                  </div> 
-                ) : (
-                  <Switch>
-                    <Route exact path="/" component={Welcome}/>
-                    <Redirect to="/"/>
-                  </Switch>
-                )
-              }
-            </div>
-          </Router>
-        </div>
-      </MuiThemeProvider>
-    )
-  }
+    render () {
+        return (
+            <MuiThemeProvider muiTheme={stellarTheme}>
+                <div>
+                    <Router>
+                        <div>
+                            {this.props.auth.isHorizonLoggedIn ? (
+                                <div>
+                                    <Header />
+                                    <Content />
+                                    <Footer />
+                                </div>
+                            ) : (
+                                <Switch>
+                                    <Route exact path="/" component={Welcome} />
+                                    <Redirect to="/" />
+                                </Switch>
+                            )}
+                        </div>
+                    </Router>
+                </div>
+            </MuiThemeProvider>
+        )
+    }
 }
 
-function mapStateToProps(state) {
-  return {
-    auth: state.auth,
-  }
+function mapStateToProps (state) {
+    return {
+        auth: state.auth,
+    }
 }
 
 export default connect(mapStateToProps)(Layout)
