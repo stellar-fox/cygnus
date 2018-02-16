a838b88db00399180fd9c56cf62b6a38b3ce0402
drmats <drmats@users.noreply.github.com>
2018-02-16 16:32:15 +0100

    More point-free style, prefer expressions over statements.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,98 +2,85 @@ import axios from "axios"
 import toml from "toml"
 
 
-// ...
-export const pubKeyAbbr = (pubKey) => {
-    return `${pubKey.slice(0,6)}-${pubKey.slice(50)}`
-}
+// TODO: convert-to/use-as module
+const StellarSdk = window.StellarSdk
 
 
 // ...
-export const utcToLocaleDateTime = (utcDateTime, includeTime=true) => {
-    if (utcDateTime !== undefined) {
-        let date = new Date(utcDateTime)
-        if (includeTime === true) {
-            return `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`
-        }
-        return `${date.toLocaleDateString()}`
-    }
-    return undefined
-}
+const domainRegex = /((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 
 // ...
-export const getAssetCode = (asset) => {
-    if (asset.asset_type === "native") {
-        return "XLM"
-    }
-    return asset.asset_code
-}
+export const pubKeyAbbr = (pubKey) =>
+    `${pubKey.slice(0, 6)}-${pubKey.slice(50)}`
 
 
 // ...
-export const formatAmount = (amount, precision=2) => {
-    return Number.parseFloat(amount).toFixed(precision)
-}
+export const utcToLocaleDateTime = (utcDateTime, includeTime = true) => (
+    (date) =>
+        includeTime ?
+            `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}` :
+            date.toLocaleDateString()
+)(utcDateTime ? new Date(utcDateTime) : new Date())
 
 
 // ...
-export const emailValid = (email) => {
-    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return regex.test(email)
-}
+export const getAssetCode = (asset) =>
+    asset.asset_type === "native" ? "XLM" : asset.asset_code
 
 
 // ...
-export const passwordValid = (password) => {
-    const regex = /^.{8,}$/
-    return regex.test(password)
-}
+export const formatAmount = (amount, precision = 2) =>
+    Number.parseFloat(amount).toFixed(precision)
 
 
 // ...
-export const passwordsMatch = (pass1, pass2) => {
-    return pass1 === pass2
-}
+export const emailValid = (email) => !!(
+    new RegExp([
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))/,
+        /@/,
+        domainRegex,
+    ].map(r => r.source).join(""))
+).test(email)
 
 
 // ...
-export const federationIsAliasOnly = (federationAddress) => {
-    const regex = /^[a-zA-Z\-0-9.@][^*]+$/
-    return (regex.test(federationAddress) === true) ? true : false
-}
+export const passwordValid = (password) => !!/^.{8,}$/.test(password)
 
 
 // ...
-export const federationAddressValid = (federationAddress) => {
-    const regex = /^[a-zA-Z\-0-9.@]+\*((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return (regex.test(federationAddress) === true) ? true : false
-}
+export const passwordsMatch = (pass1, pass2) => pass1 === pass2
 
 
 // ...
-export const federationLookup = (federationAddress) => {
-    let federationAddressDomain = federationAddress.match(/((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
-    if (federationAddressDomain !== null) {
-        return axios.get(`https://${federationAddressDomain[0]}/.well-known/stellar.toml`)
-            .then((response) => {
-                let data = toml.parse(response.data)
-                return {
+export const federationIsAliasOnly = (federationAddress) =>
+    !!/^[a-zA-Z\-0-9.@][^*]+$/.test(federationAddress)
+
+
+// ...
+export const federationAddressValid = (federationAddress) => !!(
+    new RegExp([
+        /^[a-zA-Z\-0-9.@]+\*/,
+        domainRegex,
+    ].map(r => r.source).join(""))
+).test(federationAddress)
+
+
+// ...
+export const federationLookup = (federationAddress) => (
+    (federationDomain) =>
+        federationDomain ?
+            axios
+                .get(`https://${federationDomain[0]}/.well-known/stellar.toml`)
+                .then((response) => ({
                     ok: true,
-                    endpoint: data.FEDERATION_SERVER,
-                }
-            })
-            .catch((error) => {
-                return {
-                    error: error.message,
-                }
-            })
-    }
-
-    // in case of failure - return rejected promise with error description
-    return new Promise((_res, rej) => rej(
-        new Error("Federation address domain not found...")
-    ))
-}
+                    endpoint: toml.parse(response.data).FEDERATION_SERVER,
+                })) :
+            // in case of failure - return rejected promise with error description
+            new Promise((_res, rej) =>
+                rej(new Error("Federation address domain not found..."))
+            )
+)(federationAddress.match(domainRegex))
 
 
 // ...
@@ -103,25 +90,25 @@ export const pubKeyValid = (pubKey) => {
         case pubKey.length < 56:
             validity = Object.assign(validity || {}, {
                 valid: false,
-                length: (56- pubKey.length),
-                message: (pubKey.length !== 0 ?
-                    `needs ${56 - pubKey.length} more characters` : null),
+                length: 56 - pubKey.length,
+                message:
+                    pubKey.length !== 0
+                        ? `needs ${56 - pubKey.length} more characters`
+                        : null,
             })
             break
         case pubKey.length === 56:
             try {
-                window.StellarSdk.Keypair.fromPublicKey(pubKey)
-                validity = Object.assign(validity || {},
-                    {
-                        valid: true,
-                        message: null,
-                    })
+                StellarSdk.Keypair.fromPublicKey(pubKey)
+                validity = Object.assign(validity || {}, {
+                    valid: true,
+                    message: null,
+                })
             } catch (error) {
-                validity = Object.assign(validity || {},
-                    {
-                        valid: false,
-                        message: error.message,
-                    })
+                validity = Object.assign(validity || {}, {
+                    valid: false,
+                    message: error.message,
+                })
             }
             break
         default:
