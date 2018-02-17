037eb604a8bfef77dbd60036f5797e4cf7a537f9
syntaxval <syntaxval@users.noreply.github.com>
2018-02-17 22:53:46 +0100

    ESLint.

--- src/components/TextInputField/index.js
+++ src/components/TextInputField/index.js
@@ -6,7 +6,7 @@ import TextField from "material-ui/TextField"
 export default class TextInputField extends Component {
     
     // ...
-    constructor(props) {
+    constructor (props) {
         super(props)
         this.state = {
             error: null,
@@ -16,10 +16,10 @@ export default class TextInputField extends Component {
 
 
     // ...
-    handleChange(event) {
+    handleChange (event) {
         event.persist()
         this.setState({
-            value: event.target.value
+            value: event.target.value,
         })
         if (this.props.validator !== undefined) {
             this.props.validator.call(this, this.state.value) && (
@@ -35,13 +35,13 @@ export default class TextInputField extends Component {
 
 
     // ...
-    handleOnKeyPress(event) {
+    handleOnKeyPress (event) {
         if (event.key === "Enter" && this.props.validator !== undefined) {
             let inputError = this.props.validator.call(this, this.state.value)
             
             if (inputError) {
                 this.setState({
-                    error: inputError
+                    error: inputError,
                 })
             } else {
                 if (this.props.action !== undefined) {
@@ -53,23 +53,21 @@ export default class TextInputField extends Component {
 
     
     // ...
-    render() {
+    render () {
         return (
-            <div>
-                <TextField
-                    type={this.props.type || "text"}
-                    onChange={this.handleChange.bind(this)}
-                    floatingLabelText={this.props.floatingLabelText}
-                    errorText={this.state.error}
-                    errorStyle={this.props.styles.errorStyle}
-                    underlineStyle={this.props.styles.underlineStyle}
-                    underlineFocusStyle={this.props.styles.underlineStyle}
-                    floatingLabelStyle={this.props.styles.floatingLabelStyle}
-                    floatingLabelFocusStyle={this.props.styles.floatingLabelFocusStyle}
-                    inputStyle={this.props.styles.inputStyle}
-                    onKeyPress={this.handleOnKeyPress.bind(this)}
-                />
-            </div>
+            <TextField
+                type={this.props.type || "text"}
+                onChange={this.handleChange.bind(this)}
+                floatingLabelText={this.props.floatingLabelText}
+                errorText={this.state.error}
+                errorStyle={this.props.styles.errorStyle}
+                underlineStyle={this.props.styles.underlineStyle}
+                underlineFocusStyle={this.props.styles.underlineStyle}
+                floatingLabelStyle={this.props.styles.floatingLabelStyle}
+                floatingLabelFocusStyle={this.props.styles.floatingLabelFocusStyle}
+                inputStyle={this.props.styles.inputStyle}
+                onKeyPress={this.handleOnKeyPress.bind(this)}
+            />
         )
     }
 }
--- src/frontend/themes/stellar.js
+++ src/frontend/themes/stellar.js
@@ -1,13 +1,14 @@
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import getMuiTheme from "material-ui/styles/getMuiTheme"
 
 const stellarTheme = getMuiTheme({
     tooltip: {
-        color: 'rgb(15,46,83)',
-        rippleBackgroundColor: 'rgb(244,176,4)'
+        color: "rgb(15,46,83)",
+        rippleBackgroundColor: "rgb(244,176,4)",
     },
     raisedButton: {
-        disabledColor: 'rgba(15, 46, 83, 0.5)'
-    }
+        disabledColor: "rgba(15, 46, 83, 0.5)",
+    },
+
 })
 
 export default stellarTheme
