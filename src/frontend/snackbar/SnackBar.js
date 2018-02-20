d350cc2b8fad8a7212023aca84a50ea78a80a8d8
drmats <drmats@users.noreply.github.com>
2018-02-20 14:52:02 +0100

    'SnackBar' component relint.

--- src/frontend/snackbar/SnackBar.js
+++ src/frontend/snackbar/SnackBar.js
@@ -1,26 +1,26 @@
-import React, {Component} from 'react'
-import Snackbar from 'material-ui/Snackbar'
+import React, { Component } from "react"
+import Snackbar from "material-ui/Snackbar"
 
 const styles = {
-  body: {
-    backgroundColor: 'rgb(244,176,4)',
-  },
-  content: {
-    color: 'rgb(15,46,83)',
-  }
+    body: {
+        backgroundColor: "rgb(244,176,4)",
+    },
+    content: {
+        color: "rgb(15,46,83)",
+    },
 }
 
 export default class SnackBar extends Component {
-  render() {
-    return (
-      <Snackbar
-        bodyStyle={styles.body}
-        contentStyle={styles.content}
-        open={this.props.open}
-        message={this.props.message}
-        autoHideDuration={3500}
-        onRequestClose={this.props.onRequestClose}
-      />
-    )
-  }
+    render () {
+        return (
+            <Snackbar
+                bodyStyle={styles.body}
+                contentStyle={styles.content}
+                open={this.props.open}
+                message={this.props.message}
+                autoHideDuration={3500}
+                onRequestClose={this.props.onRequestClose}
+            />
+        )
+    }
 }
