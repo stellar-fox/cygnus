d94f80ccbad398be3deb52f25e942b2b3df18257
drmats <drmats@users.noreply.github.com>
2018-02-23 10:21:36 +0100

    Stellar theme - relint.

--- src/frontend/themes/stellar.js
+++ src/frontend/themes/stellar.js
@@ -1,23 +1,29 @@
 import getMuiTheme from "material-ui/styles/getMuiTheme"
 
-const stellarTheme = getMuiTheme({
+
+
+
+// ...
+export default getMuiTheme({
+
     tooltip: {
         color: "rgb(15,46,83)",
         rippleBackgroundColor: "rgb(244,176,4)",
     },
+
     raisedButton: {
         disabledColor: "rgba(15, 46, 83, 0.5)",
     },
+
     datePicker: {
         selectColor: "rgb(15,46,83)",
         selectTextColor: "rgb(244,176,4)",
         headerColor: "rgb(15,46,83)",
         textColor: "rgb(244,176,4)",
     },
+
     flatButton: {
         primaryTextColor: "rgb(15,46,83)",
     },
 
 })
-
-export default stellarTheme
