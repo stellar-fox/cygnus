d7bf50c8fdb84c871b36a69c817b395235c567e7
drmats <drmats@users.noreply.github.com>
2018-03-08 17:06:13 +0100

    Layout: nested routing.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,12 @@
-import React, { Fragment } from "react"
+import React, { Component, Fragment } from "react"
 import { connect } from "react-redux"
 import {
-    Route,
     Redirect,
+    Route,
     Switch,
 } from "react-router-dom"
+
+import { inject } from "../../lib/utils"
 
 import LoadingModal from "../LoadingModal"
 import Welcome from "../Welcome"
@@ -14,32 +16,45 @@ import Bank from "./Bank"
 
 
 // ...
+class Layout extends Component {
+
+    // ...
+    welcomePath = this.props.basePath
+    iWelcome = inject(Welcome, { basePath: this.welcomePath, })
+    bankPath = `${this.props.basePath}bank/`
+    iBank = inject(Bank, { basePath: this.bankPath, })
+
+
+    // ...
+    render = () =>
+        <Fragment>
+            <LoadingModal />
+            <Switch>
+                <Route exact path={this.welcomePath}>
+                    {
+                        !this.props.loggedIn ?
+                            <Route component={this.iWelcome} /> :
+                            <Redirect to={this.bankPath} />
+                    }
+                </Route>
+                <Route path={this.bankPath}>
+                    {
+                        this.props.loggedIn ?
+                            <Route component={this.iBank} /> :
+                            <Redirect to={this.welcomePath} />
+                    }
+                </Route>
+                <Redirect to={this.welcomePath} />
+            </Switch>
+        </Fragment>
+
+}
+
+
+// ...
 export default connect(
     // map state to props.
     (state) => ({
         loggedIn: state.auth.isHorizonLoggedIn,
     })
-)(
-    // Layout component
-    (props) =>
-        <Fragment>
-            <LoadingModal />
-            <Switch>
-                <Route exact path="/">
-                    {
-                        !props.loggedIn ?
-                            <Route component={Welcome} /> :
-                            <Redirect to="/bank/" />
-                    }
-                </Route>
-                <Route path="/bank/">
-                    {
-                        props.loggedIn ?
-                            <Route component={Bank} /> :
-                            <Redirect to="/" />
-                    }
-                </Route>
-                <Redirect to="/" />
-            </Switch>
-        </Fragment>
-)
+)(Layout)
