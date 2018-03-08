1041f30559f437948ada39f717bc1fd006243d3d
drmats <drmats@users.noreply.github.com>
2018-03-08 17:07:13 +0100

    Bank: nested routing.

--- src/components/Layout/Bank.js
+++ src/components/Layout/Bank.js
@@ -1,8 +1,15 @@
-import React, { Fragment } from "react"
+import React, { Component, Fragment } from "react"
+import { inject } from "../../lib/utils"
+
 import {
     BankAppBar,
     BankDrawer,
 } from "./Header"
+import {
+    Redirect,
+    Route,
+    Switch,
+} from "react-router-dom"
 import Content from "./Content"
 import Footer from "./Footer"
 
@@ -10,10 +17,27 @@ import Footer from "./Footer"
 
 
 // Bank component
-export default () =>
-    <Fragment>
-        <BankAppBar />
-        <BankDrawer />
-        <Content />
-        <Footer />
-    </Fragment>
+export default class Bank extends Component {
+
+    // ...
+    balancesPath = `${this.props.basePath}balances/`
+    iBankDrawer = inject(BankDrawer, { basePath: this.props.basePath, })
+    iContent = inject(Content, { basePath: this.props.basePath, })
+
+
+    // ...
+    render = () =>
+        <Fragment>
+            <Switch>
+                <Redirect exact
+                    from={this.props.basePath}
+                    to={this.balancesPath}
+                />
+            </Switch>
+            <BankAppBar />
+            <Route component={this.iBankDrawer} />
+            <Route component={this.iContent} />
+            <Footer />
+        </Fragment>
+
+}
