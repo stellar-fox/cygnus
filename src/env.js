ddfe8260f80a963c0835edf2efcbee6f9cc765b4
drmats <drmats@users.noreply.github.com>
2018-03-12 18:01:21 +0100

    Application versioning.
    Versioning-related constants moved away to separate file.
    Session-storage key name is now depending on current application version.

---
+++ src/env.js
@@ -0,0 +1,14 @@
+// ...
+export const appName = "Stellar Fox"
+
+
+// ...
+export const appVersion = "0.1.24"
+
+
+// ...
+export const appCopyDates = "2017-2018"
+
+
+// ...
+export const ssAppStateKey = `StellarFox.${appVersion}`
