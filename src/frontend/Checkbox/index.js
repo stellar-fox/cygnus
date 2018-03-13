f893c18284fc0e1c56767f1d44513cfdff63f590
drmats <drmats@users.noreply.github.com>
2018-03-13 14:33:10 +0100

    <Checkbox>: ES7 syntax.

--- src/frontend/Checkbox/index.js
+++ src/frontend/Checkbox/index.js
@@ -4,24 +4,26 @@ import "pretty-checkbox/dist/pretty-checkbox.min.css"
 import "./index.css"
 
 
-export default class Checkbox extends Component {
-    constructor (props) {
-        super(props)
-        this.state = { checked: false, }
-    }
 
-    render () {
-        return (
-            <div className="pretty p-default p-curve">
-                <input
-                    checked={this.props.isChecked}
-                    type="checkbox"
-                    onChange={this.props.handleChange}
-                />
-                <div className="state">
-                    <label>{this.props.label}</label>
-                </div>
+
+// ...
+export default class Checkbox extends Component {
+
+    // ...
+    state = { checked: false, }
+
+
+    // ...
+    render = () =>
+        <div className="pretty p-default p-curve">
+            <input
+                checked={this.props.isChecked}
+                type="checkbox"
+                onChange={this.props.handleChange}
+            />
+            <div className="state">
+                <label>{this.props.label}</label>
             </div>
-        )
-    }
+        </div>
+
 }
