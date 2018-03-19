318526b909bd3974eb45a566463ae25c95489704
drmats <drmats@users.noreply.github.com>
2018-03-19 10:50:12 +0100

    <StellarRouter>: path-resolving helper functions.

--- src/components/StellarRouter/index.js
+++ src/components/StellarRouter/index.js
@@ -5,15 +5,34 @@ import {
     ConnectedRouter as Router
 } from "react-router-redux"
 import { Switch } from "react-router-dom"
+import resolvePathname from "resolve-pathname"
+
+
+
+
+// ...
+export const isAbsolute = (path) => path.startsWith("/")
+
+
+// ...
+export const hasTrailingSlash = (path) => path.endsWith("/")
+
+
+// ...
+export const ensureTrailingSlash = (path) =>
+    hasTrailingSlash(path) ? path : path + "/"
+
+
+// ...
+export const resolvePath = (base) => (path) =>
+    resolvePathname(path, base ? ensureTrailingSlash(base) : base)
 
 
 
 
 // ...
 export const ConnectedSwitch = connect(
-    (state) => ({
-        location: state.router.location,
-    })
+    (state) => ({ location: state.router.location, })
 )(Switch)
 
 
