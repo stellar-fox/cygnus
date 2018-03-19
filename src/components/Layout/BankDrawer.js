839c09af98c37b0666734c7228176db64cc28a20
drmats <drmats@users.noreply.github.com>
2018-03-19 16:10:02 +0100

    <BankDrawer>: code cleanups.

--- src/components/Layout/BankDrawer.js
+++ src/components/Layout/BankDrawer.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
-import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 import { NavLink } from "react-router-dom"
 import { push } from "react-router-redux"
@@ -14,146 +13,89 @@ import "./BankDrawer.css"
 
 
 
+// binds 'currentPath' state prop and 'push' dispatcher
+const navLinkConnect = connect(
+    (state) => ({ currentPath: state.router.location.pathname, }),
+    (dispatch) => ({ push: (p) => dispatch(push(p)), })
+)
+
+
+
+
 // ...
-class BalancesNavLinkCore extends Component {
-
-    // ...
-    action = (e) => {
-        e.preventDefault()
-        if (this.props.currentPath !== this.props.paths.Balances) {
-            this.props.push(this.props.paths.Balances)
-        }
-    }
-
-
-    // ...
-    render = () =>
+const BalancesNavLink = navLinkConnect(
+    ({ currentPath, paths, push, }) =>
         <NavLink
             className="menu-item"
-            onClick={this.action}
+            onClick={(e) => {
+                e.preventDefault()
+                if (currentPath !== paths.Balances) {
+                    push(paths.Balances)
+                }
+            }}
             exact
             activeClassName="active"
-            isActive={() => this.props.currentPath === this.props.paths.Balances}
-            to={this.props.paths.Balances}
+            isActive={() => currentPath === paths.Balances}
+            to={""} // required in NavLink definition but not used here
         >
             <i className="material-icons">account_balance_wallet</i>
             Balances
         </NavLink>
-
-}
-
-
-// ...
-const BalancesNavLink = connect(
-    // map state to props.
-    (state) => ({
-        currentPath: state.router.location.pathname,
-    }),
-
-    // map dispatch to props.
-    (dispatch) => bindActionCreators({
-        push,
-    }, dispatch)
-)(BalancesNavLinkCore)
+)
 
 
 
 
 // ...
-class PaymentsNavLinkCore extends Component {
-
-    // ...
-    action = (e) => {
-        e.preventDefault()
-        if (this.props.currentPath !== this.props.paths.Payments) {
-            this.props.push(this.props.paths.Payments)
-        }
-    }
-
-
-    // ...
-    render = () =>
-        this.props.accountInfo.exists ?
-            <NavLink
-                className="menu-item"
-                onClick={this.action}
-                exact
-                isActive={() => this.props.currentPath === this.props.paths.Payments}
-                activeClassName="active"
-                to={this.props.paths.Payments}
-            >
-                <i className="material-icons">payment</i>
-                Payments
-            </NavLink> :
-            null
-
-}
-
-
-// ...
-const PaymentsNavLink = connect(
-    // map state to props.
-    (state) => ({
-        accountInfo: state.accountInfo,
-        currentPath : state.router.location.pathname,
-    }),
-
-    // map dispatch to props.
-    (dispatch) => bindActionCreators({
-        push,
-    }, dispatch)
-)(PaymentsNavLinkCore)
-
-
-
-
-// ...
-class AccountNavLinkCore extends Component {
-
-    // ...
-    action = (e) => {
-        e.preventDefault()
-        if (this.props.currentPath !== this.props.paths.Account) {
-            this.props.push(this.props.paths.Account)
-        }
-    }
-
-
-    // ...
-    render = () =>
+const PaymentsNavLink = navLinkConnect(
+    ({ currentPath, paths, push, }) =>
         <NavLink
             className="menu-item"
-            onClick={this.action}
+            onClick={(e) => {
+                e.preventDefault()
+                if (currentPath !== paths.Payments) {
+                    push(paths.Payments)
+                }
+            }}
             exact
-            isActive={() => this.props.currentPath === this.props.paths.Account}
+            isActive={() => currentPath === paths.Payments}
             activeClassName="active"
-            to={this.props.paths.Account}
+            to={""} // required in NavLink definition but not used here
+        >
+            <i className="material-icons">payment</i>
+            Payments
+        </NavLink>
+)
+
+
+
+
+// ...
+const AccountNavLink = navLinkConnect(
+    ({ currentPath, paths, push, }) =>
+        <NavLink
+            className="menu-item"
+            onClick={(e) => {
+                e.preventDefault()
+                if (currentPath !== paths.Account) {
+                    push(paths.Account)
+                }
+            }}
+            exact
+            isActive={() => currentPath === paths.Account}
+            activeClassName="active"
+            to={""} // required in NavLink definition but not used here
         >
             <i className="material-icons">account_balance</i>
             Account
         </NavLink>
-
-}
-
-
-// ...
-const AccountNavLink = connect(
-    // map state to props.
-    (state) => ({
-        currentPath: state.router.location.pathname,
-    }),
-
-    // map dispatch to props.
-    (dispatch) => bindActionCreators({
-        push,
-    }, dispatch)
-)(AccountNavLinkCore)
+)
 
 
 
 
 // ...
-const style = {
+const bankDrawerStyle = {
     width: bankDrawerWidth,
     height: "calc(100% - 100px)",
     top: 65,
@@ -168,36 +110,37 @@ const style = {
 
 
 
-// ...
-class BankDrawer extends Component {
-
-    // ...
-    static propTypes = {
-        drawerOpened: PropTypes.bool.isRequired,
-        paths: PropTypes.object.isRequired,
-    }
-
-
-    // ...
-    render = () =>
-        <Drawer
-            containerStyle={style}
-            open={this.props.drawerOpened}
-        >
-            <Provide paths={this.props.paths} >
-                <BalancesNavLink />
-                <PaymentsNavLink />
-                <AccountNavLink />
-            </Provide>
-        </Drawer>
-
-}
-
-
-//
+// <BankDrawer> component
 export default connect(
-    // map state to props.
     (state) => ({
+        accountInfo: state.accountInfo,
         drawerOpened: state.ui.drawer.isOpened,
     })
-)(BankDrawer)
+)(
+    class BankDrawer extends Component {
+
+        // ...
+        static propTypes = {
+            drawerOpened: PropTypes.bool.isRequired,
+            paths: PropTypes.object.isRequired,
+        }
+
+
+        // ...
+        render = () =>
+            <Drawer
+                containerStyle={bankDrawerStyle}
+                open={this.props.drawerOpened}
+            >
+                <Provide paths={this.props.paths} >
+                    <BalancesNavLink />
+                    {
+                        this.props.accountInfo.exists ?
+                            <PaymentsNavLink /> : null
+                    }
+                    <AccountNavLink />
+                </Provide>
+            </Drawer>
+
+    }
+)
