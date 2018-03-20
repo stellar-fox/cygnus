652ac1d3469748749d4a5ee6311476fbae73feef
drmats <drmats@users.noreply.github.com>
2018-03-20 17:57:50 +0100

    <StellarRouter>: routerMiddleware reimplementation.

--- src/components/StellarRouter/index.js
+++ src/components/StellarRouter/index.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux"
 import { Switch } from "react-router-dom"
 import resolvePathname from "resolve-pathname"
+import { CALL_HISTORY_METHOD } from "react-router-redux"
 
 
 
@@ -29,3 +30,22 @@ export const resolvePath = (base) => (path) =>
 export const ConnectedSwitch = connect(
     (state) => ({ location: state.router.location, })
 )(Switch)
+
+
+
+
+/**
+ *  Reimplementation of routerMiddleware from "react-router-redux" package,
+ *  not swallowing CALL_HISTORY_METHOD action.
+ *
+ *  Time-travel / replay experiments.
+ */
+export function routerMiddleware (history) {
+    return () => next => action => {
+        if (action.type === CALL_HISTORY_METHOD) {
+            const { payload: { method, args, }, } = action
+            history[method](...args)
+        }
+        return next(action)
+    }
+}
