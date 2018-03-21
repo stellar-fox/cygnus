c33231c97c169ad33ca7c7eb697fab63bb96f9e0
syntaxval <syntaxval@users.noreply.github.com>
2018-03-21 14:42:29 +0100

    Add better error feedback to user

--- src/components/LoginManager/api.js
+++ src/components/LoginManager/api.js
@@ -10,6 +10,9 @@ export const authenticate = async (email, password) => (
                 `${config.api}/user/authenticate/`, {email, password,}
             )).data
         } catch (error) {
+            if (error.response.status >= 500) {
+                return { authenticated: false, error: error.message,}
+            }
             return error.response !== undefined ?
                 error.response.data :
                 {authenticated: false, error: error.message,}
