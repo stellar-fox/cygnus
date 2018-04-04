a0feff2562e76d2cc2b0d9c11c3c39208a3464ff
drmats <drmats@users.noreply.github.com>
2018-04-04 17:09:09 +0200

    <StellarFox>: moved 'history' and 'store' initialization here.

--- src/components/StellarFox/index.jsx
+++ src/components/StellarFox/index.jsx
@@ -1,13 +1,31 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
+import React from "react"
 import { Provider } from "react-redux"
-
-import { StellarRouter as Router } from "../StellarRouter"
+import {
+    applyMiddleware,
+    createStore,
+    combineReducers,
+} from "redux"
+import thunk from "redux-thunk"
+import createHistory from "history/createBrowserHistory"
+import { composeWithDevTools } from "redux-devtools-extension"
+import throttle from "lodash/throttle"
+import {
+    StellarRouter as Router,
+    routerMiddleware,
+} from "../StellarRouter"
+import reducers from "../../redux/reducers"
+import {
+    loadState,
+    saveState,
+} from "../../lib/statePersistence"
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
 import LoginManager from "../LoginManager"
 import AssetManager from "../AssetManager"
 import Layout from "../Layout"
-
+import {
+    appBasePath,
+    ssSaveThrottlingTime,
+} from "../StellarFox/env"
 import stellarTheme from "./theme"
 
 import "./index.css"
@@ -15,30 +33,52 @@ import "./index.css"
 
 
 
+// browser history
+export const appHistory = createHistory({
+    basename: appBasePath,
+})
+
+
+
+
+// store with router-redux integration and redux-devtools-extension
+export const appStore = (() => {
+    let s =
+        createStore(
+            combineReducers(reducers),
+            loadState(),
+            composeWithDevTools(
+                applyMiddleware(
+                    thunk,
+                    routerMiddleware(appHistory)
+                )
+            )
+        )
+
+    // save state in session storage in min. 1 sec. intervals
+    s.subscribe(
+        throttle(
+            () => saveState(s.getState()),
+            ssSaveThrottlingTime
+        )
+    )
+
+    return s
+})()
+
+
+
+
 // <StellarFox> component - application's root
-export default class StellarFox extends Component {
-
-    // ...
-    static propTypes = {
-        store: PropTypes.object.isRequired,
-        history: PropTypes.object.isRequired,
-    }
-
-
-    // ...
-    render = () => (
-        ({ store, history, }) =>
-            <Provider store={store}>
-                <Router history={history}>
-                    <MuiThemeProvider muiTheme={stellarTheme}>
-                        <LoginManager>
-                            <AssetManager>
-                                <Layout />
-                            </AssetManager>
-                        </LoginManager>
-                    </MuiThemeProvider>
-                </Router>
-            </Provider>
-    )(this.props)
-
-}
+export default () =>
+    <Provider store={appStore}>
+        <Router history={appHistory}>
+            <MuiThemeProvider muiTheme={stellarTheme}>
+                <LoginManager>
+                    <AssetManager>
+                        <Layout />
+                    </AssetManager>
+                </LoginManager>
+            </MuiThemeProvider>
+        </Router>
+    </Provider>
