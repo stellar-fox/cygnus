140edc32066a1c6e77f2cc3777982810b5c36e62
syntaxval <syntaxval@users.noreply.github.com>
2018-04-07 17:58:08 +0200

    Add createAccount and payment tx types

--- src/lib/stellar-tx.js
+++ src/lib/stellar-tx.js
@@ -1,45 +1,36 @@
 import { StellarSdk } from "./utils"
 import { config } from "../config"
-import { signTransaction } from "./ledger"
 
 StellarSdk.Network.useTestNetwork()
 const server = new StellarSdk.Server(config.horizon)
 
 
 // ...
-export const fetchAccount = async (publicKey) => {
-    try {
-        return await server.loadAccount(publicKey)
-    } catch (error) {
-        return error
-    }
-}
+export const fetchAccount = async (publicKey) =>
+    await server.loadAccount(publicKey)
 
 
 // ...
-export const createAccountTx = async (txData) => {
-    try {
-        return new StellarSdk.TransactionBuilder(
-            await fetchAccount(txData.source)
-        ).addOperation(StellarSdk.Operation.createAccount({
-            destination: txData.destination,
-            startingBalance: txData.amount,
-        })).addMemo(StellarSdk.Memo.text(txData.memo)).build()
-    } catch (error) {
-        return error
-    }
-}
+export const buildCreateAccountTx = async (txData) =>
+    new StellarSdk.TransactionBuilder(
+        await fetchAccount(txData.source)
+    ).addOperation(StellarSdk.Operation.createAccount({
+        destination: txData.destination,
+        startingBalance: txData.amount,
+    })).addMemo(StellarSdk.Memo.text(txData.memo)).build()
+
 
 // ...
-export const signTx = async (txData) => {
-    try {
-        return await signTransaction(
-            txData.bip32Path,
-            txData.publicKey,
-            txData.tx
-        )
-    } catch (error) {
-        return error
-    }
+export const buildPaymentTx = async (txData) =>
+    new StellarSdk.TransactionBuilder(
+        await fetchAccount(txData.source)
+    ).addOperation(StellarSdk.Operation.payment({
+        destination: txData.destination,
+        asset: StellarSdk.Asset.native(),
+        amount: txData.amount,
+    })).addMemo(StellarSdk.Memo.text(txData.memo)).build()
 
-}
+
+// ...
+export const broadcastTx = async (signedTx) =>
+    server.submitTransaction(signedTx)
