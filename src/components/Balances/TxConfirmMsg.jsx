5f375d2500c94377954e17f0cd2ddbc68ecbceef
syntaxval <syntaxval@users.noreply.github.com>
2018-04-09 17:54:42 +0200

    <TxConfirmMsg>: Remove redundant assetManager

--- src/components/Balances/TxConfirmMsg.jsx
+++ src/components/Balances/TxConfirmMsg.jsx
@@ -1,7 +1,5 @@
 import React, { Fragment } from "react"
-import { compose } from "redux"
 import { connect } from "react-redux"
-import { withAssetManager } from "../AssetManager"
 import { List, ListItem } from "material-ui/List"
 import {
     handleException,
@@ -11,87 +9,88 @@ import {
 
 
 
-export default compose(
-    withAssetManager,
-    connect(state => ({Balances: state.Balances,}))
-)((state) =>
-    <Fragment>
-        <div>
-            Please confirm the following info on your
-            device<he.Apos />s screen.
-        </div>
+export default connect(
+    // map state to props.
+    (state) => ({ Balances: state.Balances, })
+)(
+    ({ Balances, }) =>
+        <Fragment>
+            <div>
+                Please confirm the following info on your
+                device<he.Apos />s screen.
+            </div>
 
-        <List>
-            <ListItem
-                disabled={true}
-                primaryText="Type"
-                secondaryText={state.Balances.transactionType}
-                leftIcon={
-                    <i className="green material-icons md-icon-small">
-                        assignment_late
-                    </i>
-                }
-            />
-            <ListItem
-                disabled={true}
-                primaryText="Amount"
-                secondaryText={`${state.Balances.amount} XLM`}
-                leftIcon={
-                    <i className="green material-icons md-icon-small">
-                        account_balance_wallet
-                    </i>
-                }
-            />
-            <ListItem
-                disabled={true}
-                primaryText="Destination"
-                secondaryText={
-                    handleException(
-                        () => pubKeyAbbr(state.Balances.payee),
-                        () => "Not Available"
-                    )
-                }
-                leftIcon={
-                    <i className="green material-icons md-icon-small">
-                        local_post_office
-                    </i>
-                }
-            />
-            <ListItem
-                disabled={true}
-                primaryText="Memo"
-                secondaryText={state.Balances.memoText}
-                leftIcon={
-                    <i className="green material-icons md-icon-small">
-                        speaker_notes
-                    </i>
-                }
-            />
-            <ListItem
-                disabled={true}
-                primaryText="Fee"
-                secondaryText="0.000001 XLM"
-                leftIcon={
-                    <i className="green material-icons md-icon-small">
-                        credit_card
-                    </i>
-                }
-            />
-            <ListItem
-                disabled={true}
-                primaryText="Network"
-                secondaryText="Test"
-                leftIcon={
-                    <i className="green material-icons md-icon-small">
-                        network_check
-                    </i>
-                }
-            />
-        </List>
-        <div>
-            When you are sure it is correct press <span className="bigger">
-                <he.Check /></span>
-            on the device to sign your transaction and send it off.
-        </div>
-    </Fragment>
+            <List>
+                <ListItem
+                    disabled={true}
+                    primaryText="Type"
+                    secondaryText={Balances.transactionType}
+                    leftIcon={
+                        <i className="green material-icons md-icon-small">
+                            assignment_late
+                        </i>
+                    }
+                />
+                <ListItem
+                    disabled={true}
+                    primaryText="Amount"
+                    secondaryText={`${Balances.amount} XLM`}
+                    leftIcon={
+                        <i className="green material-icons md-icon-small">
+                            account_balance_wallet
+                        </i>
+                    }
+                />
+                <ListItem
+                    disabled={true}
+                    primaryText="Destination"
+                    secondaryText={
+                        handleException(
+                            () => pubKeyAbbr(Balances.payee),
+                            () => "Not Available"
+                        )
+                    }
+                    leftIcon={
+                        <i className="green material-icons md-icon-small">
+                            local_post_office
+                        </i>
+                    }
+                />
+                <ListItem
+                    disabled={true}
+                    primaryText="Memo"
+                    secondaryText={Balances.memoText}
+                    leftIcon={
+                        <i className="green material-icons md-icon-small">
+                            speaker_notes
+                        </i>
+                    }
+                />
+                <ListItem
+                    disabled={true}
+                    primaryText="Fee"
+                    secondaryText="0.000001 XLM"
+                    leftIcon={
+                        <i className="green material-icons md-icon-small">
+                            credit_card
+                        </i>
+                    }
+                />
+                <ListItem
+                    disabled={true}
+                    primaryText="Network"
+                    secondaryText="Test"
+                    leftIcon={
+                        <i className="green material-icons md-icon-small">
+                            network_check
+                        </i>
+                    }
+                />
+            </List>
+            <div>
+                When you are sure it is correct press <span className="bigger">
+                    <he.Check /></span>
+                on the device to sign your transaction and send it off.
+            </div>
+        </Fragment>
 )
