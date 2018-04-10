fabb2fda35bc4dc2112bdd454d38073ba0645657
syntaxval <syntaxval@users.noreply.github.com>
2018-04-10 18:47:07 +0200

    <TxConfirmMsg>: Prettify content

--- src/components/Balances/TxConfirmMsg.jsx
+++ src/components/Balances/TxConfirmMsg.jsx
@@ -8,88 +8,92 @@ import {
 } from "../../lib/utils"
 
 
-
+// ...
 export default connect(
     // map state to props.
     (state) => ({ Balances: state.Balances, })
 )(
     ({ Balances, }) =>
         <Fragment>
-            <div>
-                Please confirm the following info on your
-                device<he.Apos />s screen.
+            <div className="p-t centered">
+                Please confirm that the following info is the same on your
+                device<he.Apos />s screen:
             </div>
-
-            <List>
-                <ListItem
-                    disabled={true}
-                    primaryText="Type"
-                    secondaryText={Balances.transactionType}
-                    leftIcon={
-                        <i className="green material-icons md-icon-small">
-                            assignment_late
-                        </i>
-                    }
-                />
-                <ListItem
-                    disabled={true}
-                    primaryText="Amount"
-                    secondaryText={`${Balances.amountNative} XLM`}
-                    leftIcon={
-                        <i className="green material-icons md-icon-small">
-                            account_balance_wallet
-                        </i>
-                    }
-                />
-                <ListItem
-                    disabled={true}
-                    primaryText="Destination"
-                    secondaryText={
-                        handleException(
-                            () => pubKeyAbbr(Balances.payee),
-                            () => "Not Available"
-                        )
-                    }
-                    leftIcon={
-                        <i className="green material-icons md-icon-small">
-                            local_post_office
-                        </i>
-                    }
-                />
-                <ListItem
-                    disabled={true}
-                    primaryText="Memo"
-                    secondaryText={Balances.memoText}
-                    leftIcon={
-                        <i className="green material-icons md-icon-small">
-                            speaker_notes
-                        </i>
-                    }
-                />
-                <ListItem
-                    disabled={true}
-                    primaryText="Fee"
-                    secondaryText="0.000001 XLM"
-                    leftIcon={
-                        <i className="green material-icons md-icon-small">
-                            credit_card
-                        </i>
-                    }
-                />
-                <ListItem
-                    disabled={true}
-                    primaryText="Network"
-                    secondaryText="Test"
-                    leftIcon={
-                        <i className="green material-icons md-icon-small">
-                            network_check
-                        </i>
-                    }
-                />
-            </List>
-            <div>
-                When you are sure it is correct press <span className="bigger">
-                    <he.Check /></span>
+            <div className="f-b space-around">
+                <List>
+                    <ListItem
+                        disabled={true}
+                        primaryText="Type"
+                        secondaryText={Balances.transactionType}
+                        leftIcon={
+                            <i className="text-primary material-icons">
+                                style
+                            </i>
+                        }
+                    />
+                    <ListItem
+                        disabled={true}
+                        primaryText="Amount"
+                        secondaryText={`${Balances.amountNative} XLM`}
+                        leftIcon={
+                            <i className="text-primary material-icons">
+                                account_balance_wallet
+                            </i>
+                        }
+                    />
+                    <ListItem
+                        disabled={true}
+                        primaryText="Destination"
+                        secondaryText={
+                            handleException(
+                                () => pubKeyAbbr(Balances.payee),
+                                () => "Not Available"
+                            )
+                        }
+                        leftIcon={
+                            <i className="text-primary material-icons">
+                                gps_fixed
+                            </i>
+                        }
+                    />
+                </List>
+                <List>
+                    <ListItem
+                        disabled={true}
+                        primaryText="Memo"
+                        secondaryText={Balances.memoText === "" ?
+                            "Empty" : Balances.memoText}
+                        leftIcon={
+                            <i className="text-primary material-icons">
+                                speaker_notes
+                            </i>
+                        }
+                    />
+                    <ListItem
+                        disabled={true}
+                        primaryText="Fee"
+                        secondaryText="0.000001 XLM"
+                        leftIcon={
+                            <i className="text-primary material-icons">
+                                credit_card
+                            </i>
+                        }
+                    />
+                    <ListItem
+                        disabled={true}
+                        primaryText="Network"
+                        secondaryText="Test"
+                        leftIcon={
+                            <i className="text-primary material-icons">
+                                public
+                            </i>
+                        }
+                    />
+                </List>
+            </div>
+            <div className="p-b centered">
+                When you are sure it is correct press
+                <span className="bigger text-primary"><he.Check /></span>
                 on the device to sign your transaction and send it off.
             </div>
         </Fragment>
