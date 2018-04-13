09044499467bf02e332a35fcf14ea7d638632105
drmats <drmats@users.noreply.github.com>
2018-04-13 18:32:23 +0200

    LedgerHQ/redux: added thunk example - queryForSoftwareVersion().

--- src/redux/LedgerHQ.js
+++ src/redux/LedgerHQ.js
@@ -1,11 +1,14 @@
 import { createReducer } from "../lib/utils"
+import { getSoftwareVersion } from "../lib/ledger"
 
 
 
 
 // LedgerHQ state
 const initState = {
-
+    connected: false,
+    version: "unknown",
+    status: "idle",
 }
 
 
@@ -13,6 +16,9 @@ const initState = {
 
 // ...
 export const SET_STATE = "LedgerHQ/SET_STATE"
+export const SET_SOFTWARE_VERSION = "LedgerHQ/SET_SOFTWARE_VERSION"
+export const SET_CONNECTED = "LedgerHQ/SET_CONNECTED"
+export const SET_STATUS = "LedgerHQ/SET_STATUS"
 
 
 
@@ -23,8 +29,44 @@ export const action = {
     // ...
     setState: (state) => ({
         type: SET_STATE,
-        payload: state,
+        state,
     }),
+
+
+    // ...
+    setConnected: (state) => ({
+        type: SET_CONNECTED,
+        state,
+    }),
+
+
+    // ...
+    setSoftwareVersion: (version="unknown") => ({
+        type: SET_SOFTWARE_VERSION,
+        version,
+    }),
+
+
+    // ...
+    setStatus: (status="idle") => ({
+        type: SET_STATUS,
+        status,
+    }),
+
+
+    // ...
+    queryForSoftwareVersion: () =>
+        async (dispatch, _getState) => {
+            let version = null
+            dispatch(action.setStatus("getting version"))
+            dispatch(action.setSoftwareVersion())
+            dispatch(action.setConnected(false))
+            version = await getSoftwareVersion()
+            dispatch(action.setSoftwareVersion(version))
+            dispatch(action.setConnected(true))
+            dispatch(action.setStatus())
+            return version
+        },
 
 }
 
@@ -38,6 +80,27 @@ export const reducer = createReducer(initState)({
     [SET_STATE]: (state, action) => ({
         ...state,
         ...action.payload,
+    }),
+
+
+    // ...
+    [SET_CONNECTED]: (state, action) => ({
+        ...state,
+        connected: action.state,
+    }),
+
+
+    // ...
+    [SET_SOFTWARE_VERSION]: (state, action) => ({
+        ...state,
+        version: action.version,
+    }),
+
+
+    // ...
+    [SET_STATUS]: (state, action) => ({
+        ...state,
+        status: action.status,
     }),
 
 })
