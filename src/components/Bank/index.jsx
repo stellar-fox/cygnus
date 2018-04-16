3c520bd289f967789ccb555ef9d9d5aab0c1c828
drmats <drmats@users.noreply.github.com>
2018-04-16 15:34:38 +0200

    <Bank>: route manipulation moved to <BankContent>.

--- src/components/Bank/index.jsx
+++ src/components/Bank/index.jsx
@@ -1,11 +1,4 @@
-import React, { Component, Fragment } from "react"
-import PropTypes from "prop-types"
-import { Redirect } from "react-router-dom"
-import {
-    ConnectedSwitch as Switch,
-    resolvePath,
-    withStaticRouter,
-} from "../StellarRouter"
+import React, { Fragment } from "react"
 
 import BankAppBar from "./BankAppBar"
 import BankDrawer from "./BankDrawer"
@@ -16,48 +9,10 @@ import Footer from "../Layout/Footer"
 
 
 // <Bank> component
-export default withStaticRouter(
-    class extends Component {
-
-        // ...
-        static propTypes = {
-            match: PropTypes.object.isRequired,
-            staticRouter: PropTypes.object.isRequired,
-        }
-
-
-        // ...
-        constructor (props) {
-            super(props)
-
-            // relative resolve
-            this.rr = resolvePath(this.props.match.path)
-
-            // static paths
-            this.props.staticRouter.addPaths({
-                "Balances": this.rr("balances/"),
-                "Payments": this.rr("payments/"),
-                "Account": this.rr("account/"),
-            })
-        }
-
-
-        // ...
-        render = () => (
-            (getPath) =>
-                <Fragment>
-                    <Switch>
-                        <Redirect exact
-                            from={this.rr(".")}
-                            to={getPath("Balances")}
-                        />
-                    </Switch>
-                    <BankAppBar />
-                    <BankDrawer />
-                    <BankContent />
-                    <Footer />
-                </Fragment>
-        )(this.props.staticRouter.getPath)
-
-    }
-)
+export default () =>
+    <Fragment>
+        <BankAppBar />
+        <BankDrawer />
+        <BankContent />
+        <Footer />
+    </Fragment>
