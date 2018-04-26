2025b36d78db9a884e3a2dd1fe3de28adc4d57e9
drmats <drmats@users.noreply.github.com>
2018-04-26 17:08:03 +0200

    mui-v1 <Button>: restore changes after merge wipeout.

--- src/components/StellarFox/sfox-theme.js
+++ src/components/StellarFox/sfox-theme.js
@@ -3,11 +3,14 @@ import { createMuiTheme } from "material-ui-next/styles"
 
 
 
+// ...
 export default createMuiTheme({
+
     palette: {
         primaryColor: "rgb(15,46,83)",
         secondaryColor: "rgb(244,176,4)",
         primaryHighlight: "rgb(36,65,98)",
         secondaryHighlight: "rgb(246,190,49)",
     },
+
 })
--- src/lib/mui-v1/Button/index.jsx
+++ src/lib/mui-v1/Button/index.jsx
@@ -1,11 +1,15 @@
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
 import classNames from "classnames"
 import { withStyles } from "material-ui-next/styles"
 import Button from "material-ui-next/Button"
 
+
+
+
 // ...
-const styles = theme => ({
+const styles = (theme) => ({
+
     primary: {
         color: theme.palette.secondaryColor,
         backgroundColor: theme.palette.primaryColor,
@@ -13,6 +17,7 @@ const styles = theme => ({
             backgroundColor: theme.palette.primaryHighlight,
         },
     },
+
     secondary: {
         color: theme.palette.primaryColor,
         backgroundColor: theme.palette.secondaryColor,
@@ -20,6 +25,7 @@ const styles = theme => ({
             backgroundColor: theme.palette.secondaryHighlight,
         },
     },
+
     common: {
         boxShadow: "0 3px 7px rgba(0, 0, 0, 0.3)",
         marginRight: "0.5rem",
@@ -28,31 +34,44 @@ const styles = theme => ({
             marginRight: "0rem",
         },
     },
+
     disabled: {
         color: "rgb(244,176,4) !important",
     },
+
 })
 
-const CustomButton = (props) => {
-    const { classes, } = props
-    return (
-        <Button
-            variant="raised"
-            className={classNames(
-                props.disabled ? classes.disabled : classes[props.color],
-                classes.common,
-            )}
-            onClick={props.onClick}
-            disabled={props.disabled}
-            fullWidth={props.fullWidth}
-        >
-            {props.children ? props.children : "Button"}
-        </Button>
-    )
-}
 
-CustomButton.propTypes = {
-    classes: PropTypes.object.isRequired,
-}
 
-export default withStyles(styles)(CustomButton)
+
+// <CustomButton> component...
+export default withStyles(styles)(
+    class extends Component {
+
+        // ...
+        static propTypes = {
+            classes: PropTypes.object.isRequired,
+        }
+
+
+        // ...
+        render = () => (
+            ({ children, classes, color, disabled, fullWidth, onClick, }) =>
+                <Button
+                    variant="raised"
+                    className={
+                        classNames(
+                            disabled ? classes.disabled : classes[color],
+                            classes.common,
+                        )
+                    }
+                    onClick={onClick}
+                    disabled={disabled}
+                    fullWidth={fullWidth}
+                >
+                    { children ? children : "Button" }
+                </Button>
+        )(this.props)
+
+    }
+)
