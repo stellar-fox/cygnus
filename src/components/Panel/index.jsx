ce66df472b4094350a59208026a9d3a697908427
drmats <drmats@users.noreply.github.com>
2018-05-04 12:03:13 +0200

    <Panel>: cleanups.

--- src/components/Panel/index.jsx
+++ src/components/Panel/index.jsx
@@ -1,20 +1,13 @@
 import React from "react"
+
 import "./index.css"
 
 
 
 
 // <Panel> component
-export default ({
-    className,
-    title,
-    content,
-}) =>
-    <div className={className}>
-        <article className="message" style={{ margin: "0 auto 0 auto", }}>
-            <div className="message-header">
-                <p>{title}</p>
-            </div>
-            <div className="message-body">{content}</div>
-        </article>
-    </div>
+export default ({ title, content, }) =>
+    <article className="message">
+        <div className="message-header"><p>{title}</p></div>
+        <div className="message-body">{content}</div>
+    </article>
