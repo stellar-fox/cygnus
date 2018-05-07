055627204be59aa6d3885a49c9fcb6e002a47d02
drmats <drmats@users.noreply.github.com>
2018-05-07 10:41:44 +0200

    <Login>: <LinearProgress> - 'material-ui' -> 'material-ui-next'.

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react"
-import { connect } from "react-redux"
+import PropTypes from "prop-types"
 import { compose } from "redux"
 
 import { withLoginManager } from "../LoginManager"
@@ -8,7 +8,9 @@ import {
     passwordIsValid,
 } from "./helper"
 
-import LinearProgress from "material-ui/LinearProgress"
+import { withStyles } from "material-ui-next/styles"
+
+import { LinearProgress } from "material-ui-next"
 import InputField from "../../lib/common/InputField"
 import Button from "../../lib/mui-v1/Button"
 
@@ -18,113 +20,126 @@ import "./index.css"
 
 
 // <Login> component
-class Login extends Component {
-
-    // ...
-    state = {
-        buttonDisabled: false,
-        progressBarOpacity: "0",
-        error: "",
-    }
-
-
-    // ...
-    loginValidator = async () => {
-
-        // INVALID EMAIL FORMAT
-        if (!emailIsValid(this.email.state.value)) {
-            this.email.setState({ error: "Invalid email format.", })
-            return
-        } else {
-            this.email.setState({ error: "", })
-        }
-
-        // INVALID PASSWORD LENGTH
-        if (!passwordIsValid(this.password.state.value)) {
-            this.password.setState({ error: "Invalid password length.", })
-            return
-        } else {
-            this.password.setState({ error: "", })
-        }
-
-        // PROCEED WITH REQUEST
-        this.setState(_ => ({
-            buttonDisabled: true,
-            progressBarOpacity: "1",
-        }))
-
-        this.props.loginManager.attemptLogin(
-            this.email.state.value,
-            this.password.state.value
-        ).catch((error) => {
-            // eslint-disable-next-line no-console
-            console.log("Unknown Error: ", error)
-        }).then((auth) => {
-            if (!auth.authenticated) {
-                this.setState(_ => ({
-                    buttonDisabled: false,
-                    progressBarOpacity: "0",
-                }))
-                this.email.setState({ error: auth.error, })
-                this.password.setState({ error: auth.error, })
-            }
-        })
-
-    }
-
-
-    // ...
-    render = () =>
-        <Fragment>
-            <InputField
-                style={{ display: "block", margin: "0 auto", }}
-                name="login-email"
-                type="email"
-                placeholder="Email"
-                onEnterPress={this.loginValidator}
-                ref={(self) => { this.email = self }}
-            />
-            <InputField
-                style={{ display: "block", margin: "0 auto", }}
-                name="login-password"
-                type="password"
-                placeholder="Password"
-                onEnterPress={this.loginValidator}
-                ref={(self) => { this.password = self }}
-            />
-            <div className="p-t"></div>
-            <div className="blockcenter" style={{ width: 200, }}>
-                <LinearProgress
-                    mode="indeterminate"
-                    style={{
-                        marginBottom: "6px",
-                        background: "rgb(15,46,83)",
-                        height: "1px",
-                        opacity: this.state.progressBarOpacity,
-                    }}
-                    color="rgba(244,176,4,0.7)"
-                />
-                <Button
-                    onClick={this.loginValidator}
-                    disabled={this.state.buttonDisabled}
-                    fullWidth={true}
-                    color="secondary"
-                >
-                    Login
-                </Button>
-            </div>
-        </Fragment>
-
-}
-
-
-// ...
 export default compose(
-    withLoginManager,
-    connect(
-        // map state to props.
-        (state) => ({
-            auth: state.auth,
-        }),
-    )
-)(Login)
+    withStyles({
+
+        barRoot: {
+            height: "1px",
+            marginBottom: "6px",
+        },
+
+        colorPrimary: {
+            backgroundColor: "rgb(15, 46, 83)",
+        },
+
+        barColorPrimary: {
+            backgroundColor: "rgb(246, 190, 49)",
+        },
+
+    }),
+    withLoginManager
+)(
+    class Login extends Component {
+
+        // ...
+        static propTypes = {
+            classes: PropTypes.object.isRequired,
+        }
+
+
+        // ...
+        state = {
+            buttonDisabled: false,
+            progressBarOpacity: 0,
+            error: "",
+        }
+
+
+        // ...
+        loginValidator = async () => {
+
+            // INVALID EMAIL FORMAT
+            if (!emailIsValid(this.email.state.value)) {
+                this.email.setState({ error: "Invalid email format.", })
+                return
+            } else {
+                this.email.setState({ error: "", })
+            }
+
+            // INVALID PASSWORD LENGTH
+            if (!passwordIsValid(this.password.state.value)) {
+                this.password.setState({ error: "Invalid password length.", })
+                return
+            } else {
+                this.password.setState({ error: "", })
+            }
+
+            // PROCEED WITH REQUEST
+            this.setState(() => ({
+                buttonDisabled: true,
+                progressBarOpacity: 1,
+            }))
+
+            this.props.loginManager.attemptLogin(
+                this.email.state.value,
+                this.password.state.value
+            ).catch((error) => {
+                // eslint-disable-next-line no-console
+                console.log("Unknown Error: ", error)
+            }).then((auth) => {
+                if (!auth.authenticated) {
+                    this.setState(() => ({
+                        buttonDisabled: false,
+                        progressBarOpacity: 0,
+                    }))
+                    this.email.setState({ error: auth.error, })
+                    this.password.setState({ error: auth.error, })
+                }
+            })
+
+        }
+
+
+        // ...
+        render = () =>
+            <Fragment>
+                <InputField
+                    style={{ display: "block", margin: "0 auto", }}
+                    name="login-email"
+                    type="email"
+                    placeholder="Email"
+                    onEnterPress={this.loginValidator}
+                    ref={(self) => { this.email = self }}
+                />
+                <InputField
+                    style={{ display: "block", margin: "0 auto", }}
+                    name="login-password"
+                    type="password"
+                    placeholder="Password"
+                    onEnterPress={this.loginValidator}
+                    ref={(self) => { this.password = self }}
+                />
+                <div className="p-t"></div>
+                <div className="blockcenter" style={{ width: 200, }}>
+                    <LinearProgress
+                        variant="indeterminate"
+                        classes={{
+                            root: this.props.classes.barRoot,
+                            colorPrimary: this.props.classes.colorPrimary,
+                            barColorPrimary: this.props.classes.barColorPrimary,
+                        }}
+                        style={{ opacity: this.state.progressBarOpacity, }}
+                    />
+                    <Button
+                        onClick={this.loginValidator}
+                        disabled={this.state.buttonDisabled}
+                        fullWidth={true}
+                        color="secondary"
+                    >
+                        Login
+                    </Button>
+                </div>
+            </Fragment>
+
+    }
+)
