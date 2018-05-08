86149d8884be7554d37bf700804d41a353e46e88
syntaxval <syntaxval@users.noreply.github.com>
2018-05-08 22:35:28 +0700

    Change security pattern on check.

--- src/components/StellarFox/env.jsx
+++ src/components/StellarFox/env.jsx
@@ -197,4 +197,4 @@ export const liveNetAddr = "https://horizon.stellar.org/"
 
 
 // ...
-export const securityMsgPlaceholder = "xxxxxxxxxxxxxxxxxx"
+export const securityMsgPlaceholder = "⅏⅏⅏⅏⅏⅏⅏⅏"
