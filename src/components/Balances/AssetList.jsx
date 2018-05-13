affd3f4b46c5b8ab9d7f10bacac59b33a7a68363
syntaxval <syntaxval@users.noreply.github.com>
2018-05-13 17:43:36 +0200

    <AssetList>: Add CSS Grid to display multiple assets

--- src/components/Balances/AssetList.jsx
+++ src/components/Balances/AssetList.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react"
+import React, { Component } from "react"
 import { connect } from "react-redux"
 import { compose } from "redux"
 import { withAssetManager } from "../AssetManager"
@@ -8,6 +8,7 @@ import MD5 from "../../lib/md5"
 import { maximumTrustLimit } from "../StellarFox/env"
 import Paper from "../../lib/mui-v1/Paper"
 import Avatar from "../../lib/mui-v1/Avatar"
+import Grid from "material-ui-next/Grid"
 
 
 
@@ -16,46 +17,44 @@ import Avatar from "../../lib/mui-v1/Avatar"
 class AssetList extends Component {
 
     // ...
-    formatAssets = (assets) => assets.map((asset, index) => {
-        return (
-            <Fragment key={index}>
-                <Paper color="primary">
-                    <div className="f-b space-between cursor-pointer">
-                        <Avatar src={`https://www.gravatar.com/avatar/${
-                            MD5(asset.asset_issuer)}?s=42&d=robohash`}
-                        />
-                        <div className="p-l-small">
-                            <div className="nano p-b-nano fade-strong">
-                                Issuer: {pubKeyAbbr(asset.asset_issuer)}
-                            </div>
-                            <div className="">
-                                <span className="asset-balance">
-                                    {asset.balance}
-                                </span>
-                                <span className="asset-code">
-                                    {asset.asset_code}
-                                </span>
-                            </div>
-                            <div className="nano p-t-nano fade-strong">
-                                Trust Limit: {
-                                    new BigNumber(asset.limit)
-                                        .isLessThan(maximumTrustLimit) ?
-                                        asset.limit : "None"
-                                }
-                            </div>
+    formatAssets = (assets) => assets.map((asset, index) =>
+        <Grid item key={index} md>
+            <Paper color="primary">
+                <div className="f-b space-between cursor-pointer">
+                    <Avatar src={`https://www.gravatar.com/avatar/${
+                        MD5(asset.asset_issuer)}?s=42&d=robohash`}
+                    />
+                    <div className="p-l-small">
+                        <div className="nano p-b-nano fade-strong">
+                            Issuer: {pubKeyAbbr(asset.asset_issuer)}
+                        </div>
+                        <div className="">
+                            <span className="asset-balance">
+                                {asset.balance}
+                            </span>
+                            <span className="asset-code">
+                                {asset.asset_code}
+                            </span>
+                        </div>
+                        <div className="nano p-t-nano fade-strong nowrap">
+                            Trust Limit: {
+                                new BigNumber(asset.limit)
+                                    .isLessThan(maximumTrustLimit) ?
+                                    asset.limit : "None"
+                            }
                         </div>
                     </div>
-                </Paper>
-            </Fragment>
-        )
-    })
+                </div>
+            </Paper>
+        </Grid>
+    )
 
 
     // ...
     render = () =>
-        <div className="f-b-col">
+        <Grid container alignContent="flex-start" alignItems="center" spacing={0}>
             {this.formatAssets(this.props.assets)}
-        </div>
+        </Grid>
 
 }
 
