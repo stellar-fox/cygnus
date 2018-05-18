93b01bacb79bab30a9eeefe180e5d23bb8092e8d
syntaxval <syntaxval@users.noreply.github.com>
2018-05-18 18:50:40 +0200

    Temp mockup for contacts concept

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from "react"
+import { Grid } from "@material-ui/core"
+import ContactCard from "../ContactCard"
+import AddNew from "../ContactCard/AddNew"
 
-
+const testData = {
+    firstName: "Stellar",
+    lastName: "Fox",
+    publicKey: "GCUG5DWV7W5LH3PRGRYMVJM4SW4A4JVDB6FWOSZJV5LTFRNSHEKH2WYS",
+    paymentAddress: "fox*stellarfox.net",
+}
 
 // ...
 class Contacts extends Component {
-    render = () => <div>Contacts</div>
+    render = () =>
+        <Grid
+            container
+            alignContent="flex-start"
+            alignItems="center"
+            spacing={16}
+        >
+            <Grid item key={0} xs>
+                <AddNew />
+            </Grid>
+            <Grid item key={1} xs>
+                <ContactCard data={testData} />
+            </Grid>
+            <Grid item key={2} xs>
+                <ContactCard data={testData} />
+            </Grid>
+            <Grid item key={3} xs>
+                <ContactCard data={testData} />
+            </Grid>
+            <Grid item key={4} xs>
+                <ContactCard data={testData} />
+            </Grid>
+        </Grid>
 }
 
 
