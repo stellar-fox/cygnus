555b8b8baa60e0ce577474b025d3d0c00745499c
syntaxval <syntaxval@users.noreply.github.com>
2018-05-23 21:39:29 +0200

    Revert previous modification to gravatarLink

--- src/lib/deneb.js
+++ src/lib/deneb.js
@@ -6,11 +6,11 @@ import { config } from "../config"
 
 // TODO: rename this and its instances as it was originally just fetching
 // the gravatar link but ended up (as always) fethching everything :-)
-export const gravatarLink = async (pubkey, options) => {
+export const gravatarLink = async (pubkey) => {
 
     return Axios.get(`${config.api}/user/md5/${pubkey}`)
         .then((response) => ({
-            link: `https://www.gravatar.com/avatar/${response.data.md5}?s=${options.s || 100}`,
+            link: `https://www.gravatar.com/avatar/${response.data.md5}?s=100}`,
             firstName: response.data.first_name,
             lastName: response.data.last_name,
             email: response.data.email,
@@ -18,7 +18,7 @@ export const gravatarLink = async (pubkey, options) => {
             domain: response.data.domain,
         }))
         .catch((_error) => ({
-            link: `https://www.gravatar.com/avatar?d=${options.d}&s=${options.s || 100}`,
+            link: "https://www.gravatar.com/avatar?d=mm&s=100",
             firstName: null,
             lastName: null,
             email: null,
