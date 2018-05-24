fd635bbb3b465047ed4609c1a0fc4aa5b6fe71be
syntaxval <syntaxval@users.noreply.github.com>
2018-05-24 16:17:24 +0200

    Add select contact view drop down

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -5,7 +5,7 @@ import { withStyles } from "@material-ui/core/styles"
 import { Grid } from "@material-ui/core"
 import ContactCard from "../ContactCard"
 import AddContactForm from "./AddContactForm"
-import InputField from "../../lib/mui-v1/InputField"
+import TextField from "@material-ui/core/TextField"
 import Typography from "@material-ui/core/Typography"
 import Fuse from "fuse.js"
 import Icon from "@material-ui/core/Icon"
@@ -14,7 +14,10 @@ import Modal from "@material-ui/core/Modal"
 import { action as ContactsAction } from "../../redux/Contacts"
 import { action as ModalAction } from "../../redux/Modal"
 import { getUserContacts } from "../../lib/utils"
-
+import FormControl from "@material-ui/core/FormControl"
+import InputLabel from "@material-ui/core/InputLabel"
+import Select from "@material-ui/core/Select"
+import MenuItem from "@material-ui/core/MenuItem"
 
 
 
@@ -47,6 +50,38 @@ const styles = (theme) => ({
     outlined: {
         borderRadius: "3px",
         border: `1px solid ${theme.palette.secondary.dark}`,
+    },
+
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: "150px",
+    },
+
+    selectMenu: {
+        zIndex: 1001,
+        backgroundColor: theme.palette.secondary.light,
+    },
+
+    input: {
+        color: theme.palette.secondary.main,
+        borderBottom: `1px solid ${theme.palette.secondary.main}`,
+        "&:focus": {
+            color: theme.palette.secondary.main,
+        },
+    },
+
+    textFieldInput: {
+        color: theme.palette.secondary.main,
+        "&:hover:before": {
+            borderBottomColor: `${theme.palette.secondary.main} !important`,
+            borderBottomWidth: "1px !important",
+        },
+        "&:before": { borderBottomColor: theme.palette.secondary.main, },
+        "&:after": { borderBottomColor: theme.palette.secondary.main, },
+    },
+
+    inputMargin: {
+        margin: "0px",
     },
 
 })
@@ -103,6 +138,59 @@ const NoCards = withStyles(styles)(
 )
 
 
+// ...
+const SearchField = withStyles(styles)(
+    ({ classes, label, onChange, value, }) => <TextField
+        id="seach-by"
+        label={label}
+        value={value}
+        type="search"
+        margin="dense"
+        onChange={onChange}
+        InputProps={{
+            classes: {
+                input: classes.textFieldInput,
+                underline: classes.textFieldInput,
+            },
+        }}
+        InputLabelProps={{
+            classes: {
+                root: classes.textFieldInput,
+                marginDense: classes.inputMargin,
+                shrink: classes.shrink,
+            },
+        }}
+    />
+)
+
+
+// ...
+const SelectView = withStyles(styles)(
+    ({ classes, value, onChange, }) =>
+        <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="select-view">Select Contact View</InputLabel>
+            <Select
+                MenuProps={{
+                    PopoverClasses: {
+                        paper: classes.selectMenu,
+                    },
+                }}
+                value={value}
+                onChange={onChange}
+                inputProps={{
+                    name: "view",
+                    id: "select-view",
+                    className: classes.input,
+                }}
+            >
+                <MenuItem value={2}>Current</MenuItem>
+                <MenuItem value={1}>Pending</MenuItem>
+                <MenuItem value={3}>Declined</MenuItem>
+                <MenuItem value={4}>Blocked</MenuItem>
+            </Select>
+        </FormControl>
+)
+
 
 
 // ...
@@ -113,6 +201,7 @@ class Contacts extends Component {
         search: "",
         error: false,
         errorMessage: "",
+        selectedView: 2,
     }
 
 
@@ -169,13 +258,20 @@ class Contacts extends Component {
 
 
     // ...
+    changeView = (event) => {
+        this.setState({
+            selectedView: event.target.value,
+        })
+    }
+
+    // ...
     render = () =>
         <Fragment>
             <AddContactModal modalId={this.props.Modal.modalId}
                 visible={this.props.Modal.visible} onClose={this.hideModal}
             />
-            <div className="f-b space-between">
-                <div className="f-b-col">
+            <div className="f-b m-b space-between" style={{ alignItems: "flex-end", }}>
+                <div className="f-b-col" style={{ marginBottom: "5px", }}>
                     <Typography variant="title" color="inherit">
                         Contact Book
                     </Typography>
@@ -183,18 +279,17 @@ class Contacts extends Component {
                         Manage your financial contacts.
                     </Typography>
                 </div>
-                <div style={{ width: "100px", }}>
+                <div style={{ width: "100px", marginBottom: "8px", }}>
                     <AddContactButton onClick={this.showModal} />
                 </div>
-                <InputField
-                    id="filter-contacts"
-                    type="text"
-                    label="Search Contact Book"
-                    color="secondary"
-                    margin="dense"
-                    error={this.state.error}
-                    errorMessage={this.state.errorMessage}
-                    onChange={this.updateSearchFilter}
+                <div style={{ marginBottom: "4px", }}>
+                    <SearchField label="Search Contact Book"
+                        onChange={this.updateSearchFilter}
+                    />
+                </div>
+
+                <SelectView value={this.state.selectedView}
+                    onChange={this.changeView}
                 />
             </div>
 
