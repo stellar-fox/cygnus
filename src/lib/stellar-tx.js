4045aee57614e7c6002c2a0268d9528ca6db70eb
syntaxval <syntaxval@users.noreply.github.com>
2018-06-27 15:03:14 +0200

    Add functions needed for transaction operations parsing

--- src/lib/stellar-tx.js
+++ src/lib/stellar-tx.js
@@ -3,6 +3,8 @@ import {
     testNetAddr,
 } from "../components/StellarFox/env"
 
+import BigNumber from "bignumber.js"
+
 
 
 
@@ -81,3 +83,122 @@ export const buildPaymentTx = async (txData) =>
 // ...
 export const submitTransaction = async (signedTx, network) =>
     server(network).submitTransaction(signedTx)
+
+
+
+
+// ...
+export const displayLastBalance = (balance) => {
+    const bnBalance = new BigNumber(balance)
+
+    if (bnBalance.isEqualTo(0)) {
+        return bnBalance.toString()
+    }
+
+    if (bnBalance.isGreaterThan(0)) {
+        return `+ ${balance.toString()}`
+    }
+
+    if (bnBalance.isLessThan(0)) {
+        return `- ${balance.toString()}`
+    }
+}
+
+
+
+
+// ...
+export const displayDebit = (debit) =>
+    debit ? `- ${new BigNumber(debit).abs().toString()}` : ""
+
+
+
+
+// ...
+export const displayCredit = (credit) =>
+    credit ? `+ ${new BigNumber(credit).abs().toString()}` : ""
+
+
+
+
+// ...
+export const lastBalance = (initial, next) =>
+    new BigNumber(initial).plus(next).toString()
+
+
+
+
+// ...
+export const debit = (operations, publicKey) => {
+    const balance = operationsBalance(operations, publicKey)
+
+    if (balance.isLessThan(0)) {
+        return balance.toString()
+    }
+
+    return ""
+}
+
+
+
+
+// ...
+export const credit = (operations, publicKey) => {
+    const balance = operationsBalance(operations, publicKey)
+
+    if (balance.isGreaterThan(0)) {
+        return balance.toString()
+    }
+
+    return ""
+}
+
+
+
+
+// ...
+export const operationsBalance = (operations, publicKey) => {
+    const parsedOps = operations.map((op) => operationParse(op, publicKey))
+    let balance = new BigNumber("0.0000000")
+
+    parsedOps.forEach((op) => {
+        if (op) {
+            const match = op.match(/([+-]{1}) (.+)/)
+            if (match) {
+                let sign = match[1]
+                let amount = new BigNumber(match[2])
+                if (sign === "+") {
+                    balance = balance.plus(amount)
+                } else {
+                    balance = balance.minus(amount)
+                }
+            }
+        }
+    })
+
+    return balance
+}
+
+
+
+// ...
+export const operationParse = (operation, publicKey) => ((sign) => {
+    if (operation.startingBalance) {
+        return `${sign} ${operation.startingBalance}`
+    }
+
+    if (operation.amount) {
+        return `${sign} ${operation.amount}`
+    }
+
+})(creditOrDebit(operation, publicKey))
+
+
+
+
+// ...
+export const creditOrDebit = (operation, publicKey) => {
+    if ([ "createAccount", "payment", ].includes(operation.type)) {
+        return operation.destination === publicKey ? "+" : "-"
+    }
+}
