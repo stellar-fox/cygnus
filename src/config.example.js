d6b0084a9f63eb622f39ebcd01b84f14e205c146
syntaxval <syntaxval@users.noreply.github.com>
2018-07-08 10:30:56 +0200

    Update config example

--- src/config.example.js
+++ src/config.example.js
@@ -1,4 +1,17 @@
 export const config = {
-    api: "http://localhost:4001/api/v1",
+    api: "https://localhost:4001/api/v1",
+    apiV2: "https://localhost:4001/api/v2",
     horizon: "https://horizon-testnet.stellar.org",
+    reserve: "1",
+    firebase: {
+        apiKey: "<firebase_api_key>",
+        authDomain: "<firebase_auth_domain>",
+        databaseURL: "<firebase_db_url>",
+        projectId: "<firebase_project_id>",
+        storageBucket: "<firebase_project_storage_bucket>",
+        messagingSenderId: "<project_sender_id>",
+    },
+    mailchimp: {
+        apiKey: "<mail_chimp_api_key>",
+    },
 }
