c1be78cf15ad5016c7cfe5d9434b8bb611a1c3a7
syntaxval <syntaxval@users.noreply.github.com>
2018-07-12 15:39:53 +0200

    Add domain specified issuer information to asset display

--- src/components/Balances/AssetList.jsx
+++ src/components/Balances/AssetList.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux"
 import { compose } from "redux"
 import { BigNumber } from "bignumber.js"
 
-import { pubKeyAbbr } from "../../lib/utils"
+import { assetLookup, pubKeyAbbr } from "../../lib/utils"
 import { maximumTrustLimit } from "../StellarFox/env"
 import MD5 from "../../lib/md5"
 
@@ -13,7 +13,7 @@ import { withAssetManager } from "../AssetManager"
 
 import Paper from "../../lib/mui-v1/Paper"
 import Avatar from "../../lib/mui-v1/Avatar"
-
+import { loadAccount } from "../../lib/stellar-tx"
 
 
 
@@ -26,26 +26,89 @@ export default compose(
         (state) => ({
             assets: state.StellarAccount.assets,
             publicKey: state.StellarAccount.publicKey,
+            horizon: state.StellarAccount.horizon,
         })
     )
 )(
     class extends Component {
+
+        // ..
+        state = {
+            assetTomlInfo: [],
+        }
+
+
+        // ...
+        componentDidMount = () =>
+            this.props.assets.forEach((asset) => this.assetAvatar(asset))
+
+
+        // ...
+        assetAvatar = async (asset) => {
+            let issuingAccount = await loadAccount(
+                asset.asset_issuer, this.props.horizon
+            )
+
+            if (issuingAccount.home_domain) {
+                const assetInfo = await assetLookup(issuingAccount.home_domain)
+                if (assetInfo) {
+                    const assetIssuerInfo = assetInfo.find(
+                        (a) => a.code === asset.asset_code
+                    )
+                    this.setState({
+                        assetTomlInfo: [{
+                            asset_code: asset.asset_code,
+                            avatar: assetIssuerInfo ?
+                                assetIssuerInfo.image :
+                                `https://www.gravatar.com/avatar/${MD5(
+                                    asset.asset_issuer
+                                )}?s=42&d=mm`,
+                            decimals: assetIssuerInfo ?
+                                assetIssuerInfo.display_decimals : 7,
+                        },],
+                    })
+                }
+            }
+        }
+
+
+        // ...
+        displayAvatar = (asset) => {
+            let assetInfoObj = this.state.assetTomlInfo.find(
+                (a) => a.asset_code === asset.asset_code
+            )
+            return assetInfoObj ? assetInfoObj.avatar :
+                `https://www.gravatar.com/avatar/${MD5(
+                    asset.asset_issuer
+                )}?s=42&d=identicon`
+        }
+
+
+        // ...
+        displayBalance = (asset) => {
+            let assetInfoObj = this.state.assetTomlInfo.find(
+                (a) => a.asset_code === asset.asset_code
+            )
+
+            return assetInfoObj && assetInfoObj.decimals ?
+                new BigNumber(asset.balance).toFixed(assetInfoObj.decimals) :
+                asset.balance
+        }
+
 
         // ...
         formatAssets = (assets) => assets.map((asset, index) =>
             <Grid item key={index} xs={12} sm={12} md={4} lg={3} xl={2}>
                 <Paper color="primaryMaxWidth">
                     <div className="f-b space-between cursor-pointer">
-                        <Avatar src={`https://www.gravatar.com/avatar/${
-                            MD5(asset.asset_issuer)}?s=42&d=identicon`}
-                        />
+                        <Avatar src={this.displayAvatar(asset)} />
                         <div className="p-l-small">
                             <div className="nano p-b-nano fade-strong">
                                 Issuer: {pubKeyAbbr(asset.asset_issuer)}
                             </div>
                             <div className="">
                                 <span className="asset-balance">
-                                    {asset.balance}
+                                    {this.displayBalance(asset)}
                                 </span>
                                 <span className="asset-code">
                                     {asset.asset_code}
