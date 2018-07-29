7a28d0f0250453e64e720063722c205754d90d7a
syntaxval <syntaxval@users.noreply.github.com>
2018-07-29 21:00:27 +0200

    Cleanup config example

--- src/config.example.js
+++ src/config.example.js
@@ -11,9 +11,6 @@ export const config = {
         storageBucket: "<firebase_project_storage_bucket>",
         messagingSenderId: "<project_sender_id>",
     },
-    mailchimp: {
-        apiKey: "<mail_chimp_api_key>",
-    },
     stripe: {
         apiKey: "<stripe_api_key>",
     },
