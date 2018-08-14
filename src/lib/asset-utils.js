6573634e657057e7bc0d0d609b4f4c9d70e6a479
syntaxval <syntaxval@users.noreply.github.com>
2018-08-14 16:01:59 +0200

    Move all asset related functions to separate lib

---
+++ src/lib/asset-utils.js
@@ -0,0 +1,34 @@
+import { emptyString } from "@xcmats/js-toolbox"
+
+/**
+ * Returns utf-8 currency symbol for given asset code
+ *
+ * @returns {string}
+ */
+export const assetGlyph = (assetCode) => (
+    (codes) => codes[assetCode.toLowerCase()] ?
+        codes[assetCode.toLowerCase()] : emptyString()
+)({ eur: "€", usd: "$", aud: "$", nzd: "$", thb: "฿", pln: "zł", xlm: "⩙", })
+
+
+
+
+/**
+ * Returns long currency name for a given asset code
+ *
+ * @returns {string}
+ */
+export const assetDescription = (assetCode) => (
+    (codes) => codes[assetCode.toLowerCase()] ?
+        codes[assetCode.toLowerCase()] : emptyString()
+)({
+    eur: "European Union Euro", usd: "United States Dollar",
+    aud: "Australian Dollar", nzd: "New Zealand Dollar",
+    thb: "Thai Baht", pln: "Polish Złoty", xlm: "Stellar Lumen",
+})
+
+
+// ...
+export const opAmountisNative = (operation) =>
+    (operation.asset && operation.asset.code === "XLM")
+    || operation.startingBalance
