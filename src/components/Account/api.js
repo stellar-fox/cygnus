2c2164c69285f65c9f128c79b979ed91d3029d6c
syntaxval <syntaxval@users.noreply.github.com>
2018-08-18 13:02:37 +0200

    Add related functions for account implosion

--- src/components/Account/api.js
+++ src/components/Account/api.js
@@ -11,3 +11,25 @@ export const subscribeEmail = async (user_id, token, email) =>
             user_id, token, email,
         }
     )).status
+
+
+
+
+// ...
+export const unsubscribeEmail = async (user_id, token, email) =>
+    (await Axios.post(
+        `${config.apiV2}/user/unsubscribe-email/`, {
+            user_id, token, email,
+        }
+    )).status
+
+
+
+
+// ...
+export const implodeCloudData = async (user_id, token) =>
+    (await Axios.post(
+        `${config.apiV2}/account/implode/`, {
+            user_id, token,
+        }
+    )).status
