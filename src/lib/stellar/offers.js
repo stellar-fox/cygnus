6175f9753ee459bfe8bea64fd5fb6a6eed17c452
syntaxval <syntaxval@users.noreply.github.com>
2018-11-11 10:12:56 +0100

    Add getSellMarketPrice function

--- src/lib/stellar/offers.js
+++ src/lib/stellar/offers.js
@@ -11,34 +11,113 @@
 
 import { createServer, testNet } from "./server"
 import { Asset } from "stellar-sdk"
+import BigNumber from "bignumber.js"
 
 
 
 
 /**
- * The best price someone is willing to pay in order to buy the asset.
+ * Current offers for a base/counter asset pair. By default the best bid/ask
+ * offer is fetched.
  *
  * @async
- * @function getFirstBid
- * @param {Asset} base Base asset.
- * @param {Asset} counter Counter asset.
+ * @function getOffers
+ * @param {Asset} baseAsset Base asset.
+ * @param {Asset} counterAsset Counter asset.
  * @param {PageOptions} [opts={}]
- * @returns {Promise.<Bid>} Promise containing `Bid` object when resolved
+ * @returns {Promise.<Bid>} Promise containing `Bids` object when resolved
  * successfully.
  */
-export const getFirstBid = (
-    base,
-    counter = new Asset.native(),
+export const getOffers = (
+    baseAsset,
+    counterAsset = new Asset.native(),
     {
         limit = 1,
         horizon = testNet,
-
     } = {}
 ) =>
     createServer(horizon)
-        .orderbook(base, counter)
+        .orderbook(baseAsset, counterAsset)
         .limit(limit)
         .call()
-        .then((entry) => {
-            return entry.bids[0]
+
+
+
+
+/**
+ * Finds out the current, actual market price for the amount of asset
+ * that you have and want to market sell.
+ *
+ * @async
+ * @function getSellMarketPrice
+ * @param {Asset} baseAsset Base asset.
+ * @param {Asset} counterAsset Counter asset.
+ * @param {Number} have The amount to be sold.
+ * @param {PageOptions} [opts={}]
+ * @returns {Promise.<String>} Promise containing `String` reflecting the sell
+ * market price at that moment.
+ */
+
+export const getSellMarketPrice = async (
+    baseAsset,
+    counterAsset,
+    have,
+    {
+        limit = 1,
+        horizon = testNet,
+        increaseDepth = 5,
+    } = {}
+) => {
+
+    BigNumber.config({ DECIMAL_PLACES: 7, ROUNDING_MODE: 4 })
+
+    try {
+        const offers = await getOffers(
+            baseAsset, counterAsset, {limit, horizon}
+        )
+        let willGet = new BigNumber(0.00),
+            available = new BigNumber(0.00),
+            collected = new BigNumber(0.00)
+
+        offers.bids.some((bid) => {
+
+            collected = collected.plus(available)
+            available = available.plus(bid.amount)
+
+            if (available.isGreaterThanOrEqualTo(have)) {
+
+                willGet = willGet.plus(
+                    new BigNumber(bid.price).times(
+                        collected.minus(have).absoluteValue()
+                    )
+                )
+
+            } else {
+
+                willGet = willGet.plus(
+                    new BigNumber(bid.price).times(bid.amount)
+                )
+
+            }
+
+            return available.isGreaterThanOrEqualTo(have)
         })
+
+        if (available.isLessThan(have)) {
+            return getSellMarketPrice(
+                baseAsset,
+                counterAsset,
+                have,
+                {
+                    limit: limit + increaseDepth,
+                    horizon,
+                }
+            )
+        }
+
+        return willGet.toFixed(7)
+
+    } catch (error) {
+        return error
+    }
+}
