722d8a610a28ccba63125469f256c49a9b46aa24
syntaxval <syntaxval@users.noreply.github.com>
2019-03-08 17:22:52 +0100

    Reset App and Bank keys upon logout.

--- src/redux/Bank.js
+++ src/redux/Bank.js
@@ -5,15 +5,15 @@ import { createReducer } from "@xcmats/js-toolbox"
 
 // <Bank> component state
 const initState = {
-
     drawerVisible: true,
-
 }
 
 
 
 
 // ...
+export const SET_STATE = "@Bank/SET_STATE"
+export const RESET_STATE = "@Bank/RESET_STATE"
 export const TOGGLE_DRAWER = "@Bank/DRAWER_TOGGLE"
 
 
@@ -26,6 +26,15 @@ export const action = {
     toggleDrawer: () => ({
         type: TOGGLE_DRAWER,
     }),
+
+    // ...
+    setState: (state) => ({
+        type: SET_STATE,
+        state,
+    }),
+
+    // ...
+    resetState: () => ({ type: RESET_STATE }),
 
 }
 
@@ -40,5 +49,16 @@ export const reducer = createReducer(initState)({
         ...state,
         drawerVisible: !state.drawerVisible,
     }),
+
+
+    // ...
+    [SET_STATE]: (state, action) => ({
+        ...state,
+        ...action.state,
+    }),
+
+
+    // ...
+    [RESET_STATE]: () => initState,
 
 })
