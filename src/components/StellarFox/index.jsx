b5253074a45313f2c0c527e9da6c15a36eacadf0
syntaxval <syntaxval@users.noreply.github.com>
2019-03-08 15:28:01 +0100

    Move store and history to root index.

--- src/components/StellarFox/index.jsx
+++ src/components/StellarFox/index.jsx
@@ -1,64 +1,26 @@
 import React from "react"
-
 import { Provider } from "react-redux"
-import {
-    applyMiddleware,
-    createStore,
-    combineReducers,
-} from "redux"
-import thunk from "redux-thunk"
-import {
-    composeWithDevTools as composeWithDevTools_prod
-} from "redux-devtools-extension/developmentOnly"
-import {
-    composeWithDevTools as composeWithDevTools_dev
-} from "redux-devtools-extension"
-
-import {
-    devEnv,
-    getProcess,
-    isObject,
-    to_,
-} from "@xcmats/js-toolbox"
-
-import throttle from "lodash/throttle"
-import createHistory from "history/createBrowserHistory"
 import {
     ConnectedSwitch as Switch,
     StellarRouter as Router,
-    routerMiddleware,
 } from "../StellarRouter"
 import {
     Redirect,
     Route,
 } from "react-router-dom"
-
-import {
-    loadState,
-    saveState,
-} from "../../lib/state-persistence"
-import reducers from "../../redux"
-import {
-    dynamicImportLibs,
-    dynamicImportReducers,
-} from "../../lib/utils"
 import * as env from "./env"
-
 import { MuiThemeProvider } from "@material-ui/core/styles"
 import sFoxTheme from "../../lib/sfox-mui-theme"
 import { CssBaseline } from "@material-ui/core"
 import LegacyMuiThemeProvider from "material-ui/styles/MuiThemeProvider"
 import sFoxThemeLegacy from "../../lib/sfox-mui-theme.legacy"
-
 import AssetManager from "../AssetManager"
 import LoginManager from "../LoginManager"
 import Layout from "../Layout"
-
 import { config } from "../../config"
 import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/database"
-
 import "typeface-roboto"
 import "./index.css"
 
@@ -71,45 +33,8 @@ export const firebaseApp = firebase.initializeApp(config.firebase)
 
 
 
-// browser history
-export const history = createHistory({ /* basename: env.appBasePath, */ })
-
-
-
-
-// store with router-redux integration and redux-devtools-extension
-export const store = (() => {
-    let
-        composeWithDevTools = !devEnv()  ?
-            composeWithDevTools_prod  :  composeWithDevTools_dev,
-        s =
-            createStore(
-                combineReducers(reducers),
-                loadState(),
-                composeWithDevTools(
-                    applyMiddleware(
-                        thunk,
-                        routerMiddleware(history)
-                    )
-                )
-            )
-
-    // save state in session storage in min. 1 sec. intervals
-    s.subscribe(
-        throttle(
-            () => saveState(s.getState()),
-            env.ssSaveThrottlingTime
-        )
-    )
-
-    return s
-})()
-
-
-
-
 // <StellarFox> component - application's root
-export default () =>
+export default ({ history, store }) =>
     <Provider store={store}>
         <Router history={history}>
             <MuiThemeProvider theme={sFoxTheme}>
@@ -132,24 +57,6 @@ export default () =>
             </MuiThemeProvider>
         </Router>
     </Provider>
-
-
-
-
-// expose 'sf' dev. namespace only in dev. environment
-if (devEnv()  &&  isObject(window)) {
-    (async () => {
-        window.sf = {
-            env, history, store, React,
-            dispatch: store.dispatch,
-            ...await dynamicImportLibs(),
-            process: getProcess(),
-            r: await dynamicImportReducers(),
-        }
-        window.to_ = to_
-    })()
-}
-
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,71 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { unregister } from "./lib/caching-service-worker"
-
+import createHistory from "history/createBrowserHistory"
 import StellarFox, { env } from "./components/StellarFox"
+import {
+    applyMiddleware,
+    createStore,
+    combineReducers,
+} from "redux"
+import thunk from "redux-thunk"
+import {
+    composeWithDevTools as composeWithDevTools_prod
+} from "redux-devtools-extension/developmentOnly"
+import {
+    composeWithDevTools as composeWithDevTools_dev
+} from "redux-devtools-extension"
+import { devEnv } from "@xcmats/js-toolbox"
+
+import throttle from "lodash/throttle"
+import {
+    loadState,
+    saveState,
+} from "./lib/state-persistence"
+import reducers from "./redux"
+import { routerMiddleware } from "../src/components/StellarRouter"
+
+window.addEventListener("load", () => {
+    
+    const history = createHistory({ /* basename: env.appBasePath, */ })
+
+    const store = (() => {
+        let
+            composeWithDevTools = !devEnv() ?
+                composeWithDevTools_prod : composeWithDevTools_dev,
+            s =
+                createStore(
+                    combineReducers(reducers),
+                    loadState(),
+                    composeWithDevTools(
+                        applyMiddleware(
+                            thunk,
+                            routerMiddleware(history)
+                        )
+                    )
+                )
+
+        // save state in session storage in min. 1 sec. intervals
+        s.subscribe(
+            throttle(
+                () => saveState(s.getState()),
+                env.ssSaveThrottlingTime
+            )
+        )
+
+        return s
+    })()
+
+    // render application's root into the DOM
+    ReactDOM.render(
+        React.createElement(StellarFox, {history, store}),
+        document.getElementById(env.appRootDomId)
+    )
 
 
 
 
-// render application's root into the DOM
-ReactDOM.render(
-    React.createElement(StellarFox),
-    document.getElementById(env.appRootDomId)
-)
+    // https://bit.ly/oocache
+    unregister()
 
-
-
-
-// https://bit.ly/oocache
-unregister()
+})
