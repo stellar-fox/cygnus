1a48d3a5c14f86549864a74391bf1d0112bc06fc
syntaxval <syntaxval@users.noreply.github.com>
2019-03-11 08:36:55 +0100

    Remove some legacy material-ui components and references.

--- src/components/LoadingModal/index.jsx
+++ src/components/LoadingModal/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { connect } from "react-redux"
 import { rgb } from "../../lib/utils"
-import CircularProgress from "material-ui/CircularProgress"
+import { CircularProgress } from "@material-ui/core"
 
 import "./index.css"
 
@@ -20,30 +20,27 @@ export default connect(
     ({
         visible,
         text,
-    }) =>
-        visible ?
-            <div>
-                <div className="loading-modal-background" />
-                <div className="loading-modal">
-                    <div className="loading-modal-header">
-                        <div>&nbsp;</div>
-                        <div>&nbsp;</div>
-                    </div>
-                    <div className="loading-modal-content">
-                        <CircularProgress
-                            style={{ backgroundColor: rgb(15,46,83), }}
-                            color={rgb(244,176,4)}
-                        />
-                    </div>
-                    <div className="loading-modal-content">
-                        {text}
-                    </div>
-                    <div className="loading-modal-content">
-                        <i className="material-icons">search</i>
-                        <i className="material-icons">language</i>
-                        <i className="material-icons">fingerprint</i>
-                    </div>
-                </div>
-            </div> :
-            null
+    }) => visible && <div>
+        <div className="loading-modal-background" />
+        <div className="loading-modal">
+            <div className="loading-modal-header">
+                <div>&nbsp;</div>
+                <div>&nbsp;</div>
+            </div>
+            <div className="loading-modal-content">
+                <CircularProgress
+                    style={{ backgroundColor: rgb(15,46,83) }}
+                    color={rgb(244,176,4)}
+                />
+            </div>
+            <div className="loading-modal-content">
+                {text}
+            </div>
+            <div className="loading-modal-content">
+                <i className="material-icons">search</i>
+                <i className="material-icons">language</i>
+                <i className="material-icons">fingerprint</i>
+            </div>
+        </div>
+    </div>
 )
