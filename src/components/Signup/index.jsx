a40a18504558041e4f1d326cce2d3054d4f27093
syntaxval <syntaxval@users.noreply.github.com>
2019-03-13 13:06:29 +0100

    Refactor Signup component. Add manual account info entry.

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -22,9 +22,13 @@ import { ledgerSupportLink } from "../StellarFox/env"
 import { LinearProgress } from "@material-ui/core"
 import InputField from "../../lib/mui-v1/InputField"
 import Button from "../../lib/mui-v1/Button"
-import { signUpNewUser, clearInputErrorMessages } from "../../thunks/users"
+import {
+    clearInputErrorMessages,
+    signUpNewUser,
+} from "../../thunks/users"
 import StatusMessage from "../StatusMessage"
-
+import { publicKeyValid } from "../../lib/utils"
+import { actions as ErrorActions } from "../../redux/Errors"
 
 
 
@@ -39,15 +43,232 @@ import StatusMessage from "../StatusMessage"
  */
 
 
+
+
+// ...
 const TabContainer = (props) =>
     <Typography component="div" style={{ padding: 8 * 3 }}>
         {props.children}
     </Typography>
 
 
+
+
+// ...
+const CredentialsForm = func.compose(
+
+
+    withStyles((theme) => ({
+        linearColorPrimary: {
+            marginTop: "2px",
+            backgroundColor: theme.palette.secondary.light,
+            borderRadius: "3px",
+        },
+        linearBarColorPrimary: {
+            backgroundColor: theme.palette.secondary.dark,
+            borderRadius: "3px",
+        },
+    })),
+
+    connect(
+        (state) => ({
+            emailInputError: state.Errors.emailInputError,
+            passwordInputError: state.Errors.passwordInputError,
+            inProgress: state.Progress.signup.inProgress,
+        }),
+        (dispatch) => bindActionCreators({
+            signUpNewUser,
+            clearInputErrorMessages,
+        }, dispatch),
+    ),
+
+
+)((props) => {
+
+    const [email, setEmail] = React.useState(string.empty()),
+        [password, setPassword] = React.useState(string.empty()),
+        updateEmailInputValue = (event) => setEmail(event.target.value),
+        updatePasswordInputValue = (event) => setPassword(event.target.value),
+        signUp = () => props.signUpNewUser(
+            props.accountId, props.account, email, password
+        )
+
+
+    return <div style={{ minHeight: "350px" }}>
+        <Typography align="center" variant="caption" color="secondary">
+            Signing up with Account ID:
+        </Typography>
+        <div className="m-t-small panel-title">
+            {props.accountId && shorten(props.accountId, 11, shorten.MIDDLE, "-")}
+        </div>
+        <Typography align="center" variant="caption" color="secondary">
+            Account:
+        </Typography>
+        <div className="m-t-small panel-title">
+            {props.account}
+        </div>
+        <div className="flex-box-col items-centered content-centered">
+            <InputField
+                id="email-input"
+                type="email"
+                label="Email"
+                color="secondary"
+                error={props.emailInputError}
+                onChange={updateEmailInputValue}
+            />
+            <InputField
+                id="password-input"
+                type="password"
+                label="Password"
+                color="secondary"
+                error={props.passwordInputError}
+                onChange={updatePasswordInputValue}
+            />
+            <div>
+                <Button
+                    onClick={signUp}
+                    color="secondary"
+                    style={{ marginRight: "0px" }}
+                    disabled={props.inProgress}
+                >Sign Up</Button>
+                <LinearProgress
+                    variant="indeterminate"
+                    classes={{
+                        colorPrimary: props.classes.linearColorPrimary,
+                        barColorPrimary: props.classes.linearBarColorPrimary,
+                    }}
+                    style={{
+                        width: "100%",
+                        opacity: props.inProgress ? 1 : 0,
+                    }}
+                />
+            </div>
+            <StatusMessage className="m-t" style={{ minHeight: "20px" }} />
+        </div>
+    </div>
+})
+
+
+
+
+// ...
+const ManualEntryForm = func.compose(
+
+
+    withStyles((theme) => ({
+        linearColorPrimary: {
+            marginTop: "2px",
+            backgroundColor: theme.palette.secondary.light,
+            borderRadius: "3px",
+        },
+        linearBarColorPrimary: {
+            backgroundColor: theme.palette.secondary.dark,
+            borderRadius: "3px",
+        },
+    })),
+
+    connect(
+        (_state) => ({
+
+        }),
+        (dispatch) => bindActionCreators({
+            clearInputErrorMessages,
+            setError: ErrorActions.setOtherError,
+            signUpNewUser,
+        }, dispatch),
+    ),
+
+
+)((props) => {
+
+    const [accountId, setAccountId] = React.useState(string.empty()),
+        [account, setAccount] = React.useState(),
+        [visible, setVisible] = React.useState(false),
+        updateAccountId = (event) => setAccountId(event.target.value),
+        updateAccount = (event) => setAccount(event.target.value),
+        validateInput = () => {
+
+            if (!publicKeyValid(accountId)) {
+                props.setError("Invalid Account ID")
+                return
+            }
+
+            if (isNaN(account)) {
+                props.setError("Invalid Account")
+                return
+            }
+            props.clearInputErrorMessages()
+            setVisible(true)
+        }
+
+
+    return visible ? <CredentialsForm accountId={accountId} account={account} /> :
+        <div style={{ minHeight: "350px" }}>
+            <div className="m-t-small panel-title">
+                Please provide your Account ID.
+            </div>
+            <Typography align="center" variant="caption" color="secondary">
+                Your <em>Account ID</em> has 56 characters and
+                starts with a letter "G".
+            </Typography>
+            <div className="flex-box-col items-centered content-centered">
+                <InputField
+                    id="manual-account-id-input"
+                    type="text"
+                    label="Account ID"
+                    color="secondary"
+                    error={props.accountIdInputError}
+                    onChange={updateAccountId}
+                />
+                <InputField
+                    id="manual-account-input"
+                    type="text"
+                    label="Account"
+                    color="secondary"
+                    error={props.accountInputError}
+                    onChange={updateAccount}
+                />
+                <div>
+                    <Button
+                        onClick={validateInput}
+                        color="secondary"
+                        style={{ marginRight: "0px" }}
+                        disabled={props.inProgress}
+                    >Sign Up</Button>
+                    <LinearProgress
+                        variant="indeterminate"
+                        classes={{
+                            colorPrimary: props.classes.linearColorPrimary,
+                            barColorPrimary: props.classes.linearBarColorPrimary,
+                        }}
+                        style={{
+                            width: "100%",
+                            opacity: props.inProgress ? 1 : 0,
+                        }}
+                    />
+                </div>
+                <StatusMessage className="m-t" style={{ minHeight: "20px" }} />
+            </div>
+        </div>
+
+
+
+})
+
+
+
+
+// ...
 TabContainer.propTypes = {
     children: PropTypes.node.isRequired,
 }
+
+CredentialsForm.propTypes = {
+    account: PropTypes.string.isRequired,
+    accountId: PropTypes.string.isRequired,
+}
+
+
 
 
 /**
@@ -56,19 +277,19 @@ TabContainer.propTypes = {
  * @function Signup
  * @returns {React.ReactElement}
  */
-const Signup = ({
-    account, accountId, classes, clearInputErrorMessages, inProgress,
-    signUpNewUser, emailInputError, passwordInputError,
-}) => {
+const Signup = ({ classes, clearInputErrorMessages }) => {
+    clearInputErrorMessages()
+
     const isMobile = useMediaQuery("(max-width:960px)"),
 
-        [value, setValue] = React.useState(0),
+        [tabValue, setTabValue] = React.useState(0),
         [visible, setVisible] = React.useState(false),
-        [email, setEmail] = React.useState(string.empty()),
-        [password, setPassword] = React.useState(string.empty()),
-        
-        handleChange = (_event, newValue) => {
-            setValue(newValue)
+        [accountId, setAccuntId] = React.useState(string.empty()),
+        [account, setAccount] = React.useState(string.empty()),
+
+        handleTabChange = (_event, newValue) => {
+            setVisible(false)
+            setTabValue(newValue)
         },
 
         authViaLedger = async (ledgerData) => {
@@ -78,14 +299,10 @@ const Signup = ({
             }
             if (!ledgerData.errorCode) {
                 setVisible(true)
+                setAccuntId(ledgerData.publicKey)
+                setAccount(ledgerData.bip32Path)
             }
-        },
-
-        signUp = () => signUpNewUser(accountId, account, email, password),
-
-        updateEmailInputValue = (event) => setEmail(event.target.value),
-
-        updatePasswordInputValue = (event) => setPassword(event.target.value)
+        }
 
 
 
@@ -95,18 +312,20 @@ const Signup = ({
                 Create Account
             </Typography>
             <Typography variant="h5" color="secondary">
-                Plase choose one of available Account ID inputs.
+                Plase choose one of available sign up methods.
             </Typography>
         </div>
         <div className={isMobile ? classes.rootMobile : classes.root}>
             <AppBar position="static">
-                <Tabs value={value} onChange={handleChange}>
-                    <Tab style={{ padding: "0 0.5rem" }} icon={<img
-                        className="img-logo"
-                        src={ledgerhqlogo}
-                        width="72px"
-                        alt="LedgerHQ"
-                    />}
+                <Tabs value={tabValue} onChange={handleTabChange}>
+                    <Tab
+                        style={{ padding: "0 0.5rem" }}
+                        icon={<img
+                            className="img-logo"
+                            src={ledgerhqlogo}
+                            width="72px"
+                            alt="LedgerHQ"
+                        />}
                     />
                     <Tab
                         classes={{
@@ -117,58 +336,10 @@ const Signup = ({
                     />
                 </Tabs>
             </AppBar>
-            {value === 0 && <TabContainer>
-                <Panel title="Sign up with Ledger">
-                    {visible ?
-                        <div style={{ minHeight: "330px" }}>
-                            <Typography align="center" variant="caption" color="secondary">
-                                Signing up with Account ID:
-                            </Typography>
-                            <div className="m-t-small panel-title">
-                                {accountId && shorten(accountId, 11, shorten.MIDDLE, "-")}
-                            </div>
-                            <div className="flex-box-col items-centered content-centered">
-                                <InputField
-                                    id="email-input"
-                                    type="email"
-                                    label="Email"
-                                    color="secondary"
-                                    error={emailInputError}
-                                    onChange={updateEmailInputValue}
-                                />
-                                <InputField
-                                    id="password-input"
-                                    type="password"
-                                    label="Password"
-                                    color="secondary"
-                                    error={passwordInputError}
-                                    onChange={updatePasswordInputValue}
-                                />
-
-                                <div>
-                                    <Button
-                                        onClick={signUp}
-                                        color="secondary"
-                                        style={{ marginRight: "0px" }}
-                                        disabled={inProgress}
-                                    >Sign Up</Button>
-                                    <LinearProgress
-                                        variant="indeterminate"
-                                        classes={{
-                                            colorPrimary: classes.linearColorPrimary,
-                                            barColorPrimary: classes.linearBarColorPrimary,
-                                        }}
-                                        style={{
-                                            width: "100%",
-                                            opacity: inProgress ? 1 : 0,
-                                        }}
-                                    />
-                                </div>
-                                <StatusMessage className="m-t" />
-
-                            </div>
-                        </div> :
-                        <div style={{ minHeight: "330px" }}>
+            {tabValue === 0 && <TabContainer>
+                <Panel title="Sign up with Ledger.">
+                    {visible ? <CredentialsForm accountId={accountId} account={account} /> :
+                        <div style={{ minHeight: "350px" }}>
                             <div className="m-t-small panel-title">
                                 Account ID will be provided by your <em>Ledger</em> device.
                             </div>
@@ -179,25 +350,16 @@ const Signup = ({
                             </Typography>
                             <LedgerAuthenticator
                                 onConnected={authViaLedger}
-                                onClick={null}
                                 className="welcome-lcars-input"
+                                setRedux={false}
                             />
                         </div>
                     }
                 </Panel>
             </TabContainer>}
-            {value === 1 && <TabContainer>
-                <Panel title="Sign up by providing Account ID">
-                    <div style={{ minHeight: "330px" }}>
-                        <div className="m-t-small panel-title">
-                            Please provide your Account ID.
-                        </div>
-                        <Typography align="center" variant="caption" color="secondary">
-                            Your <em>Account ID</em> has 56 characters and
-                            starts with a letter "G".
-                        </Typography>
-                        
-                    </div>
+            {tabValue === 1 && <TabContainer>
+                <Panel title="Sign up by providing account info manually.">
+                    <ManualEntryForm />
                 </Panel>
             </TabContainer>}
         </div>
@@ -210,24 +372,13 @@ const Signup = ({
 // ...
 export default func.compose(
     withStyles((theme) => ({
-        bg: {
-            backgroundColor: theme.palette.primary.main,
-        },
-        linearColorPrimary: {
-            marginTop: "2px",
-            backgroundColor: theme.palette.secondary.light,
-            borderRadius: "3px",
-        },
-        linearBarColorPrimary: {
-            backgroundColor: theme.palette.secondary.dark,
-            borderRadius: "3px",
-        },
         root: {
             flexGrow: 1,
             backgroundColor: theme.palette.primary.light,
             borderRadius: "3px",
             maxWidth: "485px",
             minWidth: "485px",
+            minHeight: "495px",
         },
         rootMobile: {
             flexGrow: 2,
@@ -237,16 +388,10 @@ export default func.compose(
         },
     })),
     connect(
-        (state) => ({
-            accountId: state.LedgerHQ.publicKey,
-            account: state.LedgerHQ.bip32Path,
-            emailInputError: state.Errors.emailInputError,
-            emailInputErrorMessage: state.Errors.emailInputErrorMessage,
-            inProgress: state.Progress.signup.inProgress,
+        (_state) => ({
         }),
         (dispatch) => bindActionCreators({
             clearInputErrorMessages,
-            signUpNewUser,
         }, dispatch),
     ),
 )(Signup)
