890b0963478605316b7a04506cb2165822fa2023
syntaxval <syntaxval@users.noreply.github.com>
2019-03-16 15:25:44 +0100

    Add mockup for summary section.

--- src/components/Welcome/SummaryContent.jsx
+++ src/components/Welcome/SummaryContent.jsx
@@ -2,9 +2,9 @@ import React from "react"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 import { func } from "@xcmats/js-toolbox"
-import { Typography } from "@material-ui/core"
 import { unstable_useMediaQuery as useMediaQuery } from "@material-ui/core/useMediaQuery"
 import { withStyles } from "@material-ui/core/styles"
+import sflogo from "../StellarFox/static/sf-logo.svg"
 
 
 
@@ -32,17 +32,31 @@ const SummaryContent = ({ classes }) => {
     return <div className={
         isMobile ?
             `flex-box-col content-centered items-centered ${classes.bgLight}` :
-            `flex-box-row m-t-large space-around container ${classes.bgLight}`}
+            `flex-box-row space-around ${classes.bgLight}`}
     >
         <div className={isMobile ? "flex-box-col" : "flex-box-row"}>
-            <Typography variant="h3" color="primary">
-                
-            </Typography>
+            <div className="compact p-t-large p-b-large">
+                <span className="blue summary-heading">
+                    So you're new to crypto? No problem.</span> <span className="blue summary">
+                    We make it easy to send and receive payments from your friends and relatives.
+                </span>
+                <div className="blue m-t summary">
+                    From the first line of code we designed and built this wallet
+                    for newcommers and experts alike. We're excited about helping
+                    people discover the world of digital payments and expand their
+                    portfolios to include other digital assets.
+                </div>
+            </div>
         </div>
         <div className={isMobile ? "flex-box-col" : "flex-box-row"}>
-            <Typography variant="h3" color="primary">
-                
-            </Typography>
+            <div className="compact p-t-large p-b-large">
+                <img
+                    style={{ opacity: "0.2" }}
+                    src={sflogo}
+                    width="250px"
+                    alt="Stellar Fox"
+                />
+            </div>
         </div>
     </div>
 }
