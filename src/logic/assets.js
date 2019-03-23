1aa1cdf789dacbc89193ecfa9e1022e668f00d04
syntaxval <syntaxval@users.noreply.github.com>
2019-03-23 13:22:21 +0100

    Add assetToNative conversion function.

--- src/logic/assets.js
+++ src/logic/assets.js
@@ -6,14 +6,31 @@ import { string } from "@xcmats/js-toolbox"
 
 /**
  * Converts native currency units (XLM) against provided exchange rate.
- * 
+ *
  * @function nativeToAsset
  * @param {Any} amount Amount of native currency
  * @param {Any} rate Exchange rate
  * @returns {String}
  */
 export const nativeToAsset = (amount, rate) => {
-    BigNumber.config({ DECIMAL_PLACES: 4, ROUNDING_MODE: 4 })
+    BigNumber.config({ DECIMAL_PLACES: 7, ROUNDING_MODE: 4 })
     return amount !== string.empty() ?
         new BigNumber(amount).multipliedBy(rate).toFixed(2) : "0.00"
+}
+
+
+
+
+/**
+ * Converts currency units against provided native currency exchange rate.
+ *
+ * @function assetToNative
+ * @param {Any} amount Amount of asset currency
+ * @param {Any} rate Exchange rate
+ * @returns {String}
+ */
+export const assetToNative = (amount, rate) => {
+    BigNumber.config({ DECIMAL_PLACES: 7, ROUNDING_MODE: 4 })
+    return amount !== string.empty() ?
+        new BigNumber(amount).dividedBy(rate).toFixed(7) : "0.0000000"
 }
