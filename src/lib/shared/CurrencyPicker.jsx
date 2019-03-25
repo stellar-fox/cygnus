210520d97843c94513dc1b3b476bcf5da23f7110
syntaxval <syntaxval@users.noreply.github.com>
2019-03-25 15:04:27 +0100

    Format imports.

--- src/lib/shared/CurrencyPicker.jsx
+++ src/lib/shared/CurrencyPicker.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from "react"
-import { bindActionCreators, compose, } from "redux"
+import {
+    bindActionCreators,
+    compose,
+} from "redux"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
 import { withStyles } from "@material-ui/core/styles"
 import {action as BalancesAction } from "../../redux/Balances"
-import Radio from "@material-ui/core/Radio"
-import RadioGroup from "@material-ui/core/RadioGroup"
-import FormControlLabel from "@material-ui/core/FormControlLabel"
-import FormControl from "@material-ui/core/FormControl"
-import FormLabel from "@material-ui/core/FormLabel"
+import {
+    Radio,
+    RadioGroup,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+} from "@material-ui/core/Radio"
 
 
 
