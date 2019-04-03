6af60384a9a83a6146a2b696037e01ddb09c2144
syntaxval <syntaxval@users.noreply.github.com>
2019-04-03 08:06:53 +0200

    Remove legacy mui references from RegisterCard.

--- src/components/Balances/RegisterCard.jsx
+++ src/components/Balances/RegisterCard.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
+import { withStyles } from "@material-ui/core/styles"
+import { func } from "@xcmats/js-toolbox"
 import {
     Card,
     CardActions,
-} from "material-ui/Card"
-import {
     IconButton,
     Typography,
 } from "@material-ui/core"
@@ -22,38 +22,51 @@ class RegisterCard extends Component {
     dismiss = () => this.props.toggleSignupHint(false)
 
     // ...
-    render = () =>
-        <Card className="welcome-card">
-            <CardActions>
-                <div className="flex-box-row space-between items-centered">
-                    <Typography
-                        style={{ fontSize: "0.9rem", paddingLeft: "0.5rem", fontWeight: 600 }}
-                        color="secondary"
-                    >
-                        Consider <Link target="_blank"
-                            rel="noopener noreferrer" to="/signup"
-                        >signing up</Link> for a user account with our service
-                        and get convenience and security out of the box.
-                        Read about the <Link target="_blank"
-                            rel="noopener noreferrer" to="/features"
-                        >benefits</Link> of having a user account.
-                    </Typography>
-                    <IconButton onClick={this.dismiss} aria-label="Delete">
-                        <ClearIcon fontSize="small" />
-                    </IconButton>
-                </div>
+    render = () => (({ classes }) =>
+        <Card classes={{ root: classes.card }} className="welcome-card">
+            <CardActions className={classes.actions} disableActionSpacing>
+
+                <Typography
+                    style={{ paddingLeft: "0.5rem" }}
+                    color="secondary"
+                    variant="body2"
+                >
+                    Consider <Link target="_blank"
+                        rel="noopener noreferrer" to="/signup"
+                    >signing up</Link> for a user account with our service
+                    and get convenience and security out of the box.
+                    Read about the <Link target="_blank"
+                        rel="noopener noreferrer" to="/features"
+                    >benefits</Link> of having a user account.
+                </Typography>
+                <IconButton onClick={this.dismiss} aria-label="Delete">
+                    <ClearIcon fontSize="small" />
+                </IconButton>
             </CardActions>
-        </Card>
+        </Card>)(this.props)
 
 }
 
 
 // ...
-export default connect(
-    // map state to props.
-    (_state) => ({}),
-    // map dispatch to props.
-    (dispatch) => bindActionCreators({
-        toggleSignupHint: BankActions.toggleSignupHint,
-    }, dispatch)
+export default func.compose(
+    withStyles((theme) => ({
+        actions: {
+            justifyContent: "space-between",
+        },
+        card: {
+            boxSizing: "border-box",
+            borderRadius: "2px",
+        },
+        colorTextPrimary: {
+            color: theme.palette.primary.other,
+        },
+    })),
+    connect(
+        (_state) => ({}),
+        (dispatch) => bindActionCreators({
+            toggleSignupHint: BankActions.toggleSignupHint,
+        }, dispatch),
+    ),
 )(RegisterCard)
+
