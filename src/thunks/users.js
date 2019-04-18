3e1b7bf969200934eb9e847cbaa059f5eb6bbef4
syntaxval <syntaxval@users.noreply.github.com>
2019-04-18 15:08:03 +0200

    Return promise rejection on error.

--- src/thunks/users.js
+++ src/thunks/users.js
@@ -238,6 +238,7 @@ export const sendPasswordResetLink = (email) =>
             ))
         } catch (error) {
             await dispatch(setError(error))
+            return Promise.reject(error)
         }
     }
 
